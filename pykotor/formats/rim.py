5699c37142a5c271ee29e9527219506261d0b970
Cortisol <hugi.nicholas@hotmail.com>
2020-06-09 00:43:00 +0800

    rim loading

--- pykotor/formats/rim.py
+++ pykotor/formats/rim.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
 import io
-from typing import Union, List, BinaryIO
+from typing import Union, List, BinaryIO, overload
 
-from pykotor.formats.erf import ERF
 from pykotor.general.binary_reader import BinaryReader
 from pykotor.types import ResourceType, resource_types
+import pykotor.formats.erf
 
 
 class RIM:
@@ -16,7 +16,7 @@ class RIM:
     def build(self) -> bytes:
         return _RIMWriter.build(self)
 
-    def to_erf(self) -> ERF:
+    def to_erf(self) -> pykotor.formats.erf.ERF:
         pass
 
     def __init__(self):
@@ -55,8 +55,28 @@ class Resource:
 class _RIMReader:
     @staticmethod
     def load(data: bytes) -> RIM:
-        pass
-        # TODO
+        reader = BinaryReader.from_data(data)
+        rim = RIM()
+
+        file_type = reader.read_string(4)
+        file_version = reader.read_string(4)
+        reader.skip(4)
+        resource_count = reader.read_uint32()
+        table_offset = reader.read_uint32()
+        reader.skip(100)
+
+        for i in range(resource_count):
+            reader.seek(table_offset + 32 * i)
+            res_ref = reader.read_string(16)
+            res_type = ResourceType.get(reader.read_uint32())
+            reader.skip(4)
+            res_offset = reader.read_uint32()
+            res_size = reader.read_uint32()
+            reader.seek(res_offset)
+            res_data = reader.read_bytes(res_size)
+            rim.add(Resource.new(res_ref, res_type, res_data))
+
+        return rim
 
 
 class _RIMWriter:
