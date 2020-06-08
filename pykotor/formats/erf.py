59e2c5e841a9c67f319cdd5b6b4d595885ff93b6
Cortisol <hugi.nicholas@hotmail.com>
2020-06-09 00:54:27 +0800

    erf loading

--- pykotor/formats/erf.py
+++ pykotor/formats/erf.py
@@ -3,10 +3,10 @@ from __future__ import annotations
 import io
 from typing import Union, List, Type, BinaryIO
 
-from pykotor.formats.rim import RIM
 from pykotor.general.binary_reader import BinaryReader
 from pykotor.general.binary_writer import BinaryWriter
 from pykotor.types import ResourceType, resource_types
+import pykotor.formats.rim
 
 
 class ERF:
@@ -17,7 +17,7 @@ class ERF:
     def build(self) -> bytes:
         return _ERFWriter.build(self)
 
-    def to_rim(self) -> RIM:
+    def to_rim(self) -> pykotor.formats.rim.RIM:
         pass
         # TODO
 
@@ -57,8 +57,33 @@ class Resource:
 class _ERFReader(BinaryReader):
     @staticmethod
     def load(data: bytes) -> ERF:
-        pass
-        # TODO
+        reader = BinaryReader.from_data(data)
+        erf = ERF()
+
+        file_type = reader.read_string(4)
+        file_version = reader.read_string(4)
+        reader.seek(16)
+        resource_count = reader.read_uint32()
+        reader.seek(24)
+        key_list_offset = reader.read_uint32()
+        resource_list_offset = reader.read_uint32()
+
+        for i in range(resource_count):
+            reader.seek(key_list_offset + 24 * i)
+            res_ref = reader.read_string(16)
+            reader.skip(4)
+            res_type = ResourceType.get(reader.read_uint16())
+            reader.skip(2)
+
+            reader.seek(resource_list_offset + 8 * i)
+            res_offset = reader.read_uint32()
+            res_size = reader.read_uint32()
+
+            reader.seek(res_offset)
+            res_data = reader.read_bytes(res_size)
+            erf.add(Resource.new(res_ref, res_type, res_data))
+
+        return erf
 
 
 class _ERFWriter(BinaryWriter):
