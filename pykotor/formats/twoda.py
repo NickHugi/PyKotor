22b81fef6779f0a12b4258cc98d1e7fa0624a28c
Cortisol <hugi.nicholas@hotmail.com>
2020-06-09 12:21:23 +0800

    2da structure

--- pykotor/formats/twoda.py
+++ pykotor/formats/twoda.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import List, Union
+
 
 class TwoDA:
     @staticmethod
@@ -17,8 +19,59 @@ class TwoDA:
         return _2DAWriterCSV.build(self)
 
     def __init__(self):
-        pass
-        # TODO
+        # _data [ row_0:dict(), row_1:dict(), ..., row_n:dict() ]
+        self._columns: List[str] = []
+        self._data: List[dict] = []
+
+    # TODO - Error checking for all methods
+    def column_exists(self, name: str) -> bool:
+        return name in self._columns
+
+    def column_count(self) -> int:
+        return len(self._columns)
+
+    def add_column(self, name: str, default: str = "") -> None:
+        self._columns.append(name)
+        for row in self._data:
+            row[name] = default
+
+    def remove_column(self, name: str) -> None:
+        self._columns.remove(name)
+        for row in self._data:
+            del row[name]
+
+    def get_column(self, name: str) -> List[str]:
+        column_data = []
+        for row in self._data:
+            column_data.append(row[name])
+        return column_data
+
+    def row_count(self) -> int:
+        return len(self._data)
+
+    def add_row(self, at=-1, default="") -> None:
+        row_data = dict()
+        for column in self._columns:
+            row_data[column] = default
+        if at == -1:
+            self._data.append(row_data)
+        elif self.row_count() > at >= 0:
+            self._data.insert(at, row_data)
+
+    def remove_row(self, at=-1) -> None:
+        if at == -1:
+            del self._data[-1]
+        elif self.row_count() > at >= 0:
+            del self._data[at]
+
+    def get_row(self, row: int) -> dict:
+        return self._data[row].copy()
+
+    def set_data(self, column: str, row: int, value: Union[str, int]) -> None:
+        self._data[row][column] = str(value)
+
+    def get_data(self, column: str, row: int) -> str:
+        return self._data[row][column]
 
 
 class _2DAReader:
