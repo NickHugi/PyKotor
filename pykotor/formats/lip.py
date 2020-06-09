417c9aa9fb9f48460a75f711c4e8ab58eb991e5c
Cortisol <hugi.nicholas@hotmail.com>
2020-06-09 13:55:40 +0800

    Class structure

--- pykotor/formats/lip.py
+++ pykotor/formats/lip.py
@@ -1,5 +1,29 @@
 from __future__ import annotations
 
+from enum import IntEnum
+from typing import List
+
+from pykotor.general.binary_reader import BinaryReader
+
+
+class Shape(IntEnum):
+    EE = 0
+    EH = 1
+    SCHWA = 2
+    AH = 3
+    OH = 4
+    OOH = 5
+    Y = 6
+    STS = 7
+    FV = 8
+    NNG = 9
+    TH = 10
+    MPB = 11
+    TD = 12
+    JSH = 13
+    L = 14
+    KG = 15
+
 
 class LIP:
     @staticmethod
@@ -11,8 +35,21 @@ class LIP:
         _LIPWriter.build(self)
 
     def __init__(self):
-        pass
-        # TODO
+        self.length: float = 0.0
+        self.frames: List[KeyFrame] = []
+
+
+class KeyFrame:
+    @staticmethod
+    def new(time: float, shape: Shape) -> KeyFrame:
+        keyframe = KeyFrame()
+        keyframe.time = time
+        keyframe.shape = shape
+        return keyframe
+
+    def __init__(self):
+        self.time: float = 0.0
+        self.shape: Shape = Shape.EE
 
 
 class _LIPReader:
