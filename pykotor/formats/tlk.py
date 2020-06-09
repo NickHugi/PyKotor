f4a762370826d089db3169305a0729edcaeb1e2a
Cortisol <hugi.nicholas@hotmail.com>
2020-06-09 11:18:13 +0800

    tlk binary loading

--- pykotor/formats/tlk.py
+++ pykotor/formats/tlk.py
@@ -28,8 +28,21 @@ class TLK:
         self.entries: List[Entry] = []
         self.language: Language = Language.English
 
+    def add(self, entry: Entry):
+        self.entries.append(entry)
+
+    def set_language(self, language: Language):
+        self.language = language
+
 
 class Entry:
+    @staticmethod
+    def new(res_ref, text):
+        entry = Entry()
+        entry.res_ref = res_ref
+        entry.text = text
+        return entry
+
     def __init__(self):
         self.res_ref: str = ""
         self.text: str = ""
@@ -38,8 +51,31 @@ class Entry:
 class _TLKReader(BinaryReader):
     @staticmethod
     def load(data: bytes) -> TLK:
-        pass
-        # TODO
+        reader = BinaryReader.from_data(data)
+        tlk = TLK()
+
+        file_type = reader.read_string(4)
+        file_version = reader.read_string(4)
+        tlk.language_id = reader.read_uint32()
+        string_count = reader.read_uint32()
+        string_entries_offset = reader.read_uint32()
+
+        for i in range(string_count):
+            reader.seek(20 + i * 40)
+            flags = reader.read_uint32()
+            res_ref = reader.read_string(16)
+            reader.skip(4)
+            reader.skip(4)
+            offset_to_string = reader.read_uint32() + string_entries_offset
+            string_size = reader.read_uint32()
+            reader.skip(4)
+
+            reader.seek(offset_to_string)
+            text = reader.read_string(string_size)
+
+            tlk.add(Entry.new(res_ref, text))
+
+        return tlk
 
 
 class _TLKWriter(BinaryWriter):
