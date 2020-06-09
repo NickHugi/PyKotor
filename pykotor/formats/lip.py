23e94e1a62e06f845e24d2c36a0e7f93bde3e68d
Cortisol <hugi.nicholas@hotmail.com>
2020-06-09 14:00:51 +0800

    Binary reading

--- pykotor/formats/lip.py
+++ pykotor/formats/lip.py
@@ -55,8 +55,22 @@ class KeyFrame:
 class _LIPReader:
     @staticmethod
     def load(data: bytes) -> LIP:
-        pass
-        # TODO
+        lip = LIP()
+        reader = BinaryReader.from_data(data)
+
+        file_type = reader.read_string(4)
+        file_version = reader.read_string(4)
+        lip.length = reader.read_float32()
+        entry_count = reader.read_int32()
+        ''' KotOR.JS ignores 5 bytes after the header and subtracts the entry count by 1? Not sure why... '''
+
+        for i in range(entry_count):
+            time = reader.read_float32()
+            shape = Shape(reader.read_int8())
+            keyframe = KeyFrame.new(time, shape)
+            lip.frames.append(keyframe)
+
+        return lip
 
 
 class _LIPWriter:
