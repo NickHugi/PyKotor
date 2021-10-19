eb523d4390d723d7f5e33f059fbd569c442ab778
Nicholas Hugi <hugi.nicholas@hotmail.com>
2021-10-19 22:05:16 +0800

    Add tlk module

---
+++ pykotor/resource/formats/tlk.py
@@ -0,0 +1,45 @@
+"""
+This module handles classes relating to editing TLK files.
+"""
+from __future__ import annotations
+
+from typing import List
+
+from pykotor.common.language import Language
+from pykotor.common.misc import ResRef
+
+
+class TLK:
+    def __init__(self):
+        self.entries: List[TLKEntry] = []
+        self.language: Language = Language.ENGLISH
+
+    def __len__(self):
+        return len(self.entries)
+
+    def __iter__(self):
+        for stringref, entry in enumerate(self.entries):
+            yield stringref, entry
+
+    def __getitem__(self, item):
+        return self.entries[item]
+
+    def get(self, stringref: int):
+        return self.entries[stringref] if stringref in self.entries else None
+
+    def resize(self, size: int):
+        if len(self) > size:
+            self.entries = self.entries[:size]
+        else:
+            self.entries = [ResRef.from_blank() for _ in range(len(self), size)]
+
+
+class TLKEntry:
+    def __init__(self, text: str, voiceover: ResRef):
+        self.text: str = text
+        self.voiceover: ResRef = voiceover
+
+    def __eq__(self, other):
+        if isinstance(other, TLKEntry):
+            return NotImplemented
+        return other.text == self.text and other.voiceover == self.voiceover
