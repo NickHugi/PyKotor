d8f5cb21325f84de3b778644dbfc4be6ba89a132
Cortisol <hugi.nicholas@hotmail.com>
2022-02-02 14:41:43 +0800

    Write error logs to a file when application crashes

--- toolset/__main__.py
+++ toolset/__main__.py
@@ -1,11 +1,22 @@
 import sys
+import traceback
+from types import TracebackType
 
 from PyQt5.QtWidgets import QApplication
 
 from mainwindow import ToolWindow
 
+
+def onAppCrash(e: BaseException, value: str, tback: TracebackType):
+    with open("errorlog.txt", 'a') as file:
+        file.writelines(traceback.format_exception(e, value, tback))
+        file.write("\n----------------------\n")
+    sys.exit()
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    sys.excepthook = onAppCrash
     window = ToolWindow()
     window.show()
-    sys.exit(app.exec_())
+    app.exec_()
