16486ae62428d55f5ecbd5d87ca63fdb40f76439
Cortisol <hugi.nicholas@hotmail.com>
2022-02-11 22:33:55 +0800

    Display blank image for textures that fail to load

--- toolset/mainwindow.py
+++ toolset/mainwindow.py
@@ -934,6 +934,7 @@ class TextureListWorker(QThread):
 
             item, resname = self._buffer.pop()
             tpc = self._installation.texture(resname, skip_modules=True, skip_gui=False, skip_override=True)
+            tpc = TPC() if tpc is None else tpc
             width, height, rgba = tpc.convert(TPCTextureFormat.RGBA, self.bestMipmap(tpc))
             image = QImage(rgba, width, height, QImage.Format_RGBA8888)
             pixmap = QPixmap.fromImage(image).transformed(QTransform().scale(1, -1))
