08ddbaed23a18bfd9a264c135adb06ac66d816e3
Cortisol <hugi.nicholas@hotmail.com>
2022-02-11 13:41:37 +0800

    Redo the entire structure of the class

--- pykotor/common/module.py
+++ pykotor/common/module.py
@@ -41,12 +41,11 @@ T = TypeVar('T')
 
 
 class Module:
-    def __init__(self, name: str, installation: Installation, custom_capsule: Optional[Capsule] = None):
+    def __init__(self, root: str, installation: Installation, custom_capsule: Optional[Capsule] = None):
         self._installation = installation
 
         self._capsules = [custom_capsule] if custom_capsule is not None else []
-        self._capsules.extend(
-            [Capsule(installation.module_path() + module) for module in installation.module_names() if name in module])
+        self._capsules.extend([Capsule(installation.module_path() + module) for module in installation.module_names() if root in module])
 
         for capsule in self._capsules:
             if capsule.exists("module", ResourceType.IFO):
@@ -54,27 +53,42 @@ class Module:
                 self._id = ifo.root.get_resref("Mod_Entry_Area").get()
                 break
         else:
-            raise ValueError("Unable to locate module IFO file.")
+            raise ValueError("Unable to locate module IFO file for '{}'.".format(root))
 
-        self._creatures: Dict[str, ModuleResource[UTC]] = {}
-        self._placeables: Dict[str, ModuleResource[UTP]] = {}
-        self._doors: Dict[str, ModuleResource[UTD]] = {}
-        self._items: Dict[str, ModuleResource[UTI]] = {}
-        self._merchants: Dict[str, ModuleResource[UTM]] = {}
-        self._encounters: Dict[str, ModuleResource[UTE]] = {}
-        self._triggers: Dict[str, ModuleResource[UTT]] = {}
-        self._waypoints: Dict[str, ModuleResource[UTW]] = {}
-        self._sounds: Dict[str, ModuleResource[UTS]] = {}
-        self._dialog: Dict[str, ModuleResource[DLG]] = {}
-        self._paths: Dict[str, ModuleResource[PTH]] = {}
-        self._scripts: Dict[str, ModuleResource] = {}
-        self._textures: Dict[str, ModuleResource[TPC]] = {}
+        self.creatures: Dict[str, ModuleResource[UTC]] = {}
+        self.placeables: Dict[str, ModuleResource[UTP]] = {}
+        self.doors: Dict[str, ModuleResource[UTD]] = {}
+        self.items: Dict[str, ModuleResource[UTI]] = {}
+        self.merchants: Dict[str, ModuleResource[UTM]] = {}
+        self.encounters: Dict[str, ModuleResource[UTE]] = {}
+        self.triggers: Dict[str, ModuleResource[UTT]] = {}
+        self.waypoints: Dict[str, ModuleResource[UTW]] = {}
+        self.sounds: Dict[str, ModuleResource[UTS]] = {}
+        self.dialog: Dict[str, ModuleResource[DLG]] = {}
+        self.scripts: Dict[str, ModuleResource] = {}
+        self.textures: Dict[str, ModuleResource[TPC]] = {}
 
-        self.layout: ModuleResource[LYT] = ModuleResource(self._id, ResourceType.LYT, installation, None, [])
-        self.visibility: ModuleResource[VIS] = ModuleResource(self._id, ResourceType.VIS, installation, None, [])
-        self.dynamic: ModuleResource[GIT] = ModuleResource(self._id, ResourceType.GIT, installation, None, [])
-        self.static: ModuleResource[ARE] = ModuleResource(self._id, ResourceType.ARE, installation, None, [])
-        self.info: ModuleResource[IFO] = ModuleResource("module", ResourceType.IFO, installation, None, [])
+        self.layout: ModuleResource[LYT] = ModuleResource(self._id, ResourceType.LYT, installation, [])
+        self.visibility: ModuleResource[VIS] = ModuleResource(self._id, ResourceType.VIS, installation, [])
+        self.dynamic: ModuleResource[GIT] = ModuleResource(self._id, ResourceType.GIT, installation, [])
+        self.static: ModuleResource[ARE] = ModuleResource(self._id, ResourceType.ARE, installation, [])
+        self.path: ModuleResource[PTH] = ModuleResource(self._id, ResourceType.PTH, installation, [])
+        self.info: ModuleResource[IFO] = ModuleResource("module", ResourceType.IFO, installation, [])
+
+        self.restype_to_resources = {
+            ResourceType.UTC: self.creatures,
+            ResourceType.UTP: self.placeables,
+            ResourceType.UTD: self.doors,
+            ResourceType.UTI: self.items,
+            ResourceType.UTM: self.merchants,
+            ResourceType.UTE: self.encounters,
+            ResourceType.UTT: self.triggers,
+            ResourceType.UTW: self.waypoints,
+            ResourceType.UTS: self.sounds,
+            ResourceType.DLG: self.dialog,
+            ResourceType.NCS: self.scripts,
+            ResourceType.TPC: self.textures
+        }
 
         self.reload_resources()
 
@@ -98,77 +112,112 @@ class Module:
         return roota + rootb
 
     def reload_resources(self):
-        self.layout = ModuleResource(self._id, ResourceType.LYT, self._installation, None, [])
-        self.dynamic = ModuleResource(self._id, ResourceType.GIT, self._installation, None, [])
-        self.static = ModuleResource(self._id, ResourceType.ARE, self._installation, None, [])
-        self.info = ModuleResource("module", ResourceType.IFO, self._installation, None, [])
-
-        load_list = {
-            ResourceType.UTC: self._creatures,
-            ResourceType.UTP: self._placeables,
-            ResourceType.UTD: self._doors,
-            ResourceType.UTI: self._items,
-            ResourceType.UTM: self._merchants,
-            ResourceType.UTE: self._encounters,
-            ResourceType.UTT: self._triggers,
-            ResourceType.UTW: self._waypoints,
-            ResourceType.UTS: self._sounds,
-            ResourceType.DLG: self._dialog,
-            ResourceType.PTH: self._paths,
-            ResourceType.NCS: self._scripts,
-            ResourceType.TPC: self._textures
-        }
+        utc_list = set()
+        utp_list = set()
+        utd_list = set()
+        uti_list = set()
+        utm_list = set()
+        ute_list = set()
+        utt_list = set()
+        utw_list = set()
+        uts_list = set()
+        dlg_list = set()
+        ncs_list = set()
 
         for capsule in self._capsules:
             for resource in capsule:
-                if resource.restype() in load_list:
-                    dictionary = load_list[resource.restype()]
-                    if resource.resname() not in dictionary:
-                        dictionary[resource.resname()] = ModuleResource(resource.resname(), resource.restype(),
-                                                                        self._installation, capsule.path(), [capsule.path()])
-                    else:
-                        dictionary[resource.resname()].locations.append(capsule.path())
+                resname = resource.resname()
+                restype = resource.restype()
 
-                if resource.restype() == ResourceType.LYT:
-                    self.layout.active = capsule.path() if self.layout.active is None else self.layout.active
-                    self.layout.locations.append(capsule.path())
-
-                if resource.restype() == ResourceType.VIS:
-                    self.visibility.active = capsule.path() if self.layout.active is None else self.visibility.active
-                    self.visibility.locations.append(capsule.path())
-
-                if resource.restype() == ResourceType.IFO:
-                    self.info.active = capsule.path() if self.info.active is None else self.info.active
-                    self.info.locations.append(capsule.path())
-
-                if resource.restype() == ResourceType.ARE:
-                    self.static.active = capsule.path() if self.static.active is None else self.static.active
-                    self.static.locations.append(capsule.path())
+                if restype in self.restype_to_resources:
+                    self.add_locations(resname, restype, [capsule.path()])
 
                 if resource.restype() == ResourceType.GIT:
-                    self.dynamic.active = capsule.path() if self.dynamic.active is None else self.dynamic.active
-                    self.dynamic.locations.append(capsule.path())
+                    git = construct_git(load_gff(capsule.resource(self._id, ResourceType.GIT)))
+                    utc_list = utc_list.union(set([creature.resref.get() for creature in git.creatures]))
+                    utp_list = utp_list.union(set([placeable.resref.get() for placeable in git.placeables]))
+                    utd_list = utd_list.union(set([door.resref.get() for door in git.doors]))
+                    utm_list = utm_list.union(set([merchant.resref.get() for merchant in git.stores]))
+                    ute_list = ute_list.union(set([encounter.resref.get() for encounter in git.encounters]))
+                    utt_list = utt_list.union(set([trigger.resref.get() for trigger in git.triggers]))
+                    utw_list = utw_list.union(set([waypoint.resref.get() for waypoint in git.waypoints]))
+                    uts_list = uts_list.union(set([sound.resref.get() for sound in git.sounds]))
 
-        lyts = self._installation.locate(self._id, ResourceType.LYT, capsules=self._capsules, skip_modules=True,
-                                         skip_textures=True)
-        for location in lyts:
-            self.layout.active = location.filepath if self.layout.active is None else self.layout.active
-            self.layout.locations.append(location.filepath)
+        for utc_resname in utc_list:
+            search = self._installation.locate(utc_resname, ResourceType.UTC, skip_modules=True, skip_textures=True)
+            self.add_locations(utc_resname, ResourceType.UTC, [result.filepath for result in search])
 
-        viss = self._installation.locate(self._id, ResourceType.VIS, capsules=self._capsules, skip_modules=True,
-                                         skip_textures=True)
-        for location in viss:
-            self.visibility.active = location.filepath if self.visibility.active is None else self.visibility.active
-            self.visibility.locations.append(location.filepath)
+        for utp_resname in utp_list:
+            search = self._installation.locate(utp_resname, ResourceType.UTP, skip_modules=True, skip_textures=True)
+            self.add_locations(utp_resname, ResourceType.UTP, [result.filepath for result in search])
+
+        for utd_resname in utd_list:
+            search = self._installation.locate(utd_resname, ResourceType.UTD, skip_modules=True, skip_textures=True)
+            self.add_locations(utd_resname, ResourceType.UTD, [result.filepath for result in search])
+
+        for uti_resname in uti_list:
+            search = self._installation.locate(uti_resname, ResourceType.UTI, skip_modules=True, skip_textures=True)
+            self.add_locations(uti_resname, ResourceType.UTI, [result.filepath for result in search])
+
+        for utm_resname in utm_list:
+            search = self._installation.locate(utm_resname, ResourceType.UTM, skip_modules=True, skip_textures=True)
+            self.add_locations(utm_resname, ResourceType.UTM, [result.filepath for result in search])
+
+        for ute_resname in ute_list:
+            search = self._installation.locate(ute_resname, ResourceType.UTE, skip_modules=True, skip_textures=True)
+            self.add_locations(ute_resname, ResourceType.UTE, [result.filepath for result in search])
+
+        for utt_resname in utt_list:
+            search = self._installation.locate(utt_resname, ResourceType.UTT, skip_modules=True, skip_textures=True)
+            self.add_locations(utt_resname, ResourceType.UTT, [result.filepath for result in search])
+
+        for utw_resname in utw_list:
+            search = self._installation.locate(utw_resname, ResourceType.UTW, skip_modules=True, skip_textures=True)
+            self.add_locations(utw_resname, ResourceType.UTW, [result.filepath for result in search])
+
+        for uts_resname in uts_list:
+            search = self._installation.locate(uts_resname, ResourceType.UTS, skip_modules=True, skip_textures=True)
+            self.add_locations(uts_resname, ResourceType.UTS, [result.filepath for result in search])
+
+        self.info = ModuleResource("module", ResourceType.IFO, self._installation, [])
+        search = self._installation.locate("module", ResourceType.IFO, capsules=self._capsules, skip_modules=True, skip_textures=True)
+        self.info.add_locations([result.filepath for result in search])
+
+        self.static = ModuleResource(self._id, ResourceType.ARE, self._installation, [])
+        search = self._installation.locate(self._id, ResourceType.ARE, capsules=self._capsules, skip_modules=True, skip_textures=True)
+        self.static.add_locations([result.filepath for result in search])
+
+        self.dynamic = ModuleResource(self._id, ResourceType.GIT, self._installation, [])
+        search = self._installation.locate(self._id, ResourceType.GIT, capsules=self._capsules, skip_modules=True, skip_textures=True)
+        self.dynamic.add_locations([result.filepath for result in search])
+
+        self.path = ModuleResource(self._id, ResourceType.PTH, self._installation, [])
+        search = self._installation.locate(self._id, ResourceType.PTH, capsules=self._capsules, skip_modules=True, skip_textures=True)
+        self.path.add_locations([result.filepath for result in search])
+
+        self.layout = ModuleResource(self._id, ResourceType.LYT, self._installation, [])
+        search = self._installation.locate(self._id, ResourceType.LYT, capsules=self._capsules, skip_modules=True, skip_textures=True)
+        self.layout.add_locations([result.filepath for result in search])
+
+        self.visibility = ModuleResource(self._id, ResourceType.VIS, self._installation, [])
+        search = self._installation.locate(self._id, ResourceType.VIS, capsules=self._capsules, skip_modules=True, skip_textures=True)
+        self.visibility.add_locations([result.filepath for result in search])
+
+    def add_locations(self, resname: str, restype: ResourceType, locations: List[str]):
+        resource_list = self.restype_to_resources[restype]
+        if resname in resource_list:
+            resource_list[resname].add_locations(locations)
+        else:
+            resource_list[resname] = ModuleResource(resname, restype, self._installation, locations)
 
 
 class ModuleResource(Generic[T]):
-    def __init__(self, resname: str, restype: ResourceType, installation: Installation,  active: Optional[str],
+    def __init__(self, resname: str, restype: ResourceType, installation: Installation,
                  locations: List[str]):
         self.resname: str = resname
         self._installation = installation
         self.restype: ResourceType = restype
-        self.active: str = active
+        self.active: Optional[str] = locations[0] if len(locations) > 0 else None
         self.locations: List[str] = locations
 
     def resource(self) -> T:
@@ -205,3 +254,7 @@ class ModuleResource(Generic[T]):
         else:
             data = BinaryReader.load_file(self.active)
             return conversions[self.restype](data)
+
+    def add_locations(self, filepaths: List[str]) -> None:
+        self.locations.extend([filepath for filepath in filepaths if filepath not in self.locations])
+        self.active = self.locations[0] if self.active is None and self.locations else self.active
