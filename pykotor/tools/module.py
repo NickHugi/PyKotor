53509d0c7b3c73207023eb908aaff080a2250c72
Cortisol <hugi.nicholas@hotmail.com>
2022-02-11 13:41:56 +0800

    Add method to clone module

---
+++ pykotor/tools/module.py
@@ -0,0 +1,170 @@
+from pykotor.resource.formats.lyt.auto import write_lyt
+
+from pykotor.tools import model
+
+from pykotor.resource.formats.tpc import TPCTextureFormat, TPC, write_tpc
+from pykotor.resource.generics.git import dismantle_git
+from pykotor.resource.generics.uts import dismantle_uts
+from pykotor.resource.generics.utp import dismantle_utp
+from pykotor.resource.generics.utd import dismantle_utd
+from pykotor.resource.generics.are import dismantle_are
+from pykotor.resource.generics.ifo import dismantle_ifo
+
+from pykotor.resource.type import ResourceType, FileFormat
+
+from pykotor.common.language import LocalizedString
+from pykotor.common.module import Module
+
+from pykotor.extract.installation import Installation
+from pykotor.resource.formats.erf import ERF, ERFType, write_erf
+from pykotor.resource.formats.gff import write_gff
+from pykotor.resource.formats.vis import write_vis
+
+
+def clone_module(root: str, identifier: str, prefix: str, name: str, installation: Installation, *,
+                 copyTextures: bool = False, copyLightmaps: bool = False, keepDoors: bool = False,
+                 keepPlaceables: bool = False, keepSounds: bool = False, keepPathing: bool = False) -> None:
+    installation = installation
+    root = root
+    identifier = identifier
+
+    oldModule = Module(root, installation)
+    newModule = ERF(ERFType.MOD)
+
+    ifo = oldModule.info.resource()
+    oldIdentifier = ifo.identifier.get()
+    ifo.identifier.set(identifier)
+    ifo.mod_name = LocalizedString.from_english(identifier.upper())
+    ifo.tag = identifier.upper()
+    ifo.area_name.set(identifier)
+    ifo_data = bytearray()
+    write_gff(dismantle_ifo(ifo), ifo_data)
+    newModule.set("module", ResourceType.IFO, ifo_data)
+
+    are = oldModule.static.resource()
+    are.name = LocalizedString.from_english(name)
+    are_data = bytearray()
+    write_gff(dismantle_are(are), are_data)
+    newModule.set(identifier, ResourceType.ARE, are_data)
+
+    git = oldModule.dynamic.resource()
+
+    if keepDoors:
+        for i, door in enumerate(git.doors):
+            oldResname = door.resref.get()
+            newResname = "{}_dor{}".format(identifier, i)
+            door.resref.set(newResname)
+            door.tag = newResname
+
+            utd = oldModule.doors[oldResname].resource()
+            data = bytearray()
+            write_gff(dismantle_utd(utd), data)
+            newModule.set(newResname, ResourceType.UTD, data)
+    else:
+        git.doors = []
+
+    if keepPlaceables:
+        for i, placeable in enumerate(git.placeables):
+            oldResname = placeable.resref.get()
+            newResname = "{}_plc{}".format(identifier, i)
+            placeable.resref.set(newResname)
+            placeable.tag = newResname
+
+            utp = oldModule.placeables[oldResname].resource()
+            data = bytearray()
+            write_gff(dismantle_utp(utp), data)
+            newModule.set(newResname, ResourceType.UTP, data)
+    else:
+        git.placeables = []
+
+    if keepSounds:
+        for i, sound in enumerate(git.sounds):
+            oldResname = sound.resref.get()
+            newResname = "{}_snd{}".format(identifier, i)
+            sound.resref.set(newResname)
+            sound.tag = newResname
+
+            uts = oldModule.sounds[oldResname].resource()
+            data = bytearray()
+            write_gff(dismantle_uts(uts), data)
+            newModule.set(newResname, ResourceType.UTS, data)
+    else:
+        git.sounds = []
+
+    git.creatures = []
+    git.encounters = []
+    git.sounds = []
+    git.stores = []
+    git.triggers = []
+    git.waypoints = []
+    git.cameras = []
+    git_data = bytearray()
+    write_gff(dismantle_git(git), git_data)
+    newModule.set(identifier, ResourceType.GIT, git_data)
+
+    lyt = oldModule.layout.resource()
+    vis = oldModule.visibility.resource()
+
+    newLightmaps = {}
+    newTextures = {}
+
+    for room in lyt.rooms:
+        oldModelName = room.model
+        newModelName = oldModelName.lower().replace(oldIdentifier, identifier)
+
+        room.model = newModelName
+        vis.rename_room(oldModelName, newModelName)
+
+        mdlData = installation.resource(oldModelName, ResourceType.MDL).data
+        mdxData = installation.resource(oldModelName, ResourceType.MDX).data
+        wokData = installation.resource(oldModelName, ResourceType.WOK).data
+
+        if copyTextures:
+            for texture in model.list_textures(mdlData):
+                if texture not in newTextures:
+                    newTextureName = prefix + texture[3:]
+                    newTextures[texture] = newTextureName
+
+                    tpc = installation.texture(texture)
+                    rgba = tpc.convert(TPCTextureFormat.RGBA)
+
+                    tga = TPC()
+                    tga.set(rgba.width, rgba.height, [rgba.data], TPCTextureFormat.RGBA)
+
+                    tga_data = bytearray()
+                    write_tpc(tga, tga_data, FileFormat.TGA)
+                    newModule.set(newTextureName, ResourceType.TGA, tga_data)
+            mdlData = model.change_textures(mdlData, newTextures)
+
+        if copyLightmaps:
+            for lightmap in model.list_lightmaps(mdlData):
+                if lightmap not in newLightmaps:
+                    newLightmapName = "{}_lm_{}".format(identifier, len(newLightmaps.keys()))
+                    newLightmaps[lightmap] = newLightmapName
+
+                    tpc = installation.texture(lightmap, skip_chitin=False)
+                    rgba = tpc.convert(TPCTextureFormat.RGBA)
+
+                    tga = TPC()
+                    tga.set(rgba.width, rgba.height, [rgba.data], TPCTextureFormat.RGBA)
+
+                    tga_data = bytearray()
+                    write_tpc(tga, tga_data, FileFormat.TGA)
+                    newModule.set(newLightmapName, ResourceType.TGA, tga_data)
+            mdlData = model.change_lightmaps(mdlData, newLightmaps)
+
+        mdlData = model.rename(mdlData, newModelName)
+        newModule.set(newModelName, ResourceType.MDL, mdlData)
+        newModule.set(newModelName, ResourceType.MDX, mdxData)
+        newModule.set(newModelName, ResourceType.WOK, wokData)
+
+    lyt_data = bytearray()
+    write_lyt(lyt, lyt_data)
+    newModule.set(identifier, ResourceType.LYT, lyt_data)
+
+    vis_data = bytearray()
+    write_vis(vis, vis_data)
+    newModule.set(identifier, ResourceType.VIS, vis_data)
+
+    filepath = installation.module_path() + identifier + ".mod"
+    write_erf(newModule, filepath)
