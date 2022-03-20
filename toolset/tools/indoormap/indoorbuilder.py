b636f431bf62f337cd126ece12df922b61c64714
Nicholas Hugi <hugi.nicholas@hotmail.com>
2022-03-20 19:21:19 +0800

    Moving building kit list to indoorkit module

--- toolset/tools/indoormap/indoorbuilder.py
+++ toolset/tools/indoormap/indoorbuilder.py
@@ -19,7 +19,7 @@ from pykotor.resource.type import ResourceType
 
 from data.installation import HTInstallation
 from tools.indoormap import indoorbuilder_ui
-from tools.indoormap.indoorkit import KitComponent, KitComponentHook, Kit, KitDoor
+from tools.indoormap.indoorkit import KitComponent, KitComponentHook, Kit, KitDoor, load_kits
 from tools.indoormap.indoormap import IndoorMap, IndoorMapRoom
 from tools.indoormap.indoorsettings import IndoorMapSettings
 
@@ -51,60 +51,7 @@ class IndoorMapBuilder(QMainWindow):
         self.ui.mapRenderer.mouseScrolled.connect(self.onMouseScrolled)
 
     def _setupKits(self) -> None:
-        kits_path: str = "C:/Users/hugin/Documents/Apps/HolocronToolset/kits"
-        for filename in [filename for filename in os.listdir(kits_path) if filename.endswith(".json")]:
-            kit_json = json.loads(BinaryReader.load_file("{}/{}".format(kits_path, filename)))
-            kit = Kit(kit_json["name"])
-            kit_identifier = kit_json["id"]
-
-            always_path = "{}/{}/always".format(kits_path, filename[:-5])
-            for always_file in os.listdir(always_path):
-                kit.always[always_file] = BinaryReader.load_file("{}/{}".format(always_path, always_file))
-
-            textures_path = "{}/{}/textures".format(kits_path, filename[:-5])
-            for texture_file in [filename for filename in os.listdir(textures_path) if filename.endswith(".tga")]:
-                texture = texture_file[:-4]
-                kit.textures[texture] = BinaryReader.load_file("{}/{}.tga".format(textures_path, texture))
-                txi_path = "{}/{}.txi".format(textures_path, texture)
-                kit.txis[texture] = BinaryReader.load_file(txi_path) if os.path.exists(txi_path) else b''
-
-            lightmaps_path = "{}/{}/lightmaps".format(kits_path, filename[:-5])
-            for lightmap_file in [filename for filename in os.listdir(lightmaps_path) if filename.endswith(".tga")]:
-                lightmap = lightmap_file[:-4]
-                kit.lightmaps[lightmap] = BinaryReader.load_file("{}/{}.tga".format(lightmaps_path, lightmap))
-                txi_path = "{}/{}.txi".format(lightmaps_path, lightmap)
-                kit.txis[lightmap] = BinaryReader.load_file(txi_path) if os.path.exists(txi_path) else b''
-
-            for door_json in kit_json["doors"]:
-                utdK1 = read_utd("{}/{}/{}.utd".format(kits_path, kit_identifier, door_json["utd_k1"]))
-                utdK2 = read_utd("{}/{}/{}.utd".format(kits_path, kit_identifier, door_json["utd_k2"]))
-                width = door_json["width"]
-                height = door_json["height"]
-                door = KitDoor(utdK1, utdK2, width, height)
-                kit.doors.append(door)
-
-            for component_json in kit_json["components"]:
-                name = component_json["name"]
-                component_identifier = component_json["id"]
-
-                image = QImage("{}/{}/{}.png".format(kits_path, kit_identifier, component_identifier)).mirrored()
-
-                bwm = read_bwm("{}/{}/{}.wok".format(kits_path, kit_identifier, component_identifier))
-                mdl = BinaryReader.load_file("{}/{}/{}.mdl".format(kits_path, kit_identifier, component_identifier))
-                mdx = BinaryReader.load_file("{}/{}/{}.mdx".format(kits_path, kit_identifier, component_identifier))
-                component = KitComponent(kit, name, image, bwm, mdl, mdx)
-
-                for hook_json in component_json["doorhooks"]:
-                    position = Vector3(hook_json["x"], hook_json["y"], hook_json["z"])
-                    rotation = hook_json["rotation"]
-                    door = kit.doors[hook_json["door"]]
-                    edge = hook_json["edge"]
-                    hook = KitComponentHook(position, rotation, edge, door)
-                    component.hooks.append(hook)
-
-                kit.components.append(component)
-
-            self._kits.append(kit)
+        self._kits = load_kits("C:/Users/hugin/Documents/Apps/HolocronToolset/kits")
 
         for kit in self._kits:
             self.ui.kitSelect.addItem(kit.name, kit)
--- toolset/tools/indoormap/indoorkit.py
+++ toolset/tools/indoormap/indoorkit.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
+import json
+import os
 from typing import List, Dict
 
 from PyQt5.QtGui import QImage
 from pykotor.common.geometry import Vector3
 from pykotor.common.misc import CaseInsensitiveDict
-from pykotor.resource.formats.bwm import BWM
-from pykotor.resource.generics.utd import UTD
+from pykotor.common.stream import BinaryReader
+from pykotor.resource.formats.bwm import BWM, read_bwm
+from pykotor.resource.generics.utd import UTD, read_utd
 
 
 class Kit:
@@ -46,3 +49,64 @@ class KitDoor:
         self.utdK2: UTD = utdK2
         self.width: float = width
         self.priority: float = height
+
+
+def load_kits(path: str) -> List[Kit]:
+    kits = []
+
+    kits_path = path
+    for filename in [filename for filename in os.listdir(kits_path) if filename.endswith(".json")]:
+        kit_json = json.loads(BinaryReader.load_file("{}/{}".format(kits_path, filename)))
+        kit = Kit(kit_json["name"])
+        kit_identifier = kit_json["id"]
+
+        always_path = "{}/{}/always".format(kits_path, filename[:-5])
+        for always_file in os.listdir(always_path):
+            kit.always[always_file] = BinaryReader.load_file("{}/{}".format(always_path, always_file))
+
+        textures_path = "{}/{}/textures".format(kits_path, filename[:-5])
+        for texture_file in [filename for filename in os.listdir(textures_path) if filename.endswith(".tga")]:
+            texture = texture_file[:-4]
+            kit.textures[texture] = BinaryReader.load_file("{}/{}.tga".format(textures_path, texture))
+            txi_path = "{}/{}.txi".format(textures_path, texture)
+            kit.txis[texture] = BinaryReader.load_file(txi_path) if os.path.exists(txi_path) else b''
+
+        lightmaps_path = "{}/{}/lightmaps".format(kits_path, filename[:-5])
+        for lightmap_file in [filename for filename in os.listdir(lightmaps_path) if filename.endswith(".tga")]:
+            lightmap = lightmap_file[:-4]
+            kit.lightmaps[lightmap] = BinaryReader.load_file("{}/{}.tga".format(lightmaps_path, lightmap))
+            txi_path = "{}/{}.txi".format(lightmaps_path, lightmap)
+            kit.txis[lightmap] = BinaryReader.load_file(txi_path) if os.path.exists(txi_path) else b''
+
+        for door_json in kit_json["doors"]:
+            utdK1 = read_utd("{}/{}/{}.utd".format(kits_path, kit_identifier, door_json["utd_k1"]))
+            utdK2 = read_utd("{}/{}/{}.utd".format(kits_path, kit_identifier, door_json["utd_k2"]))
+            width = door_json["width"]
+            height = door_json["height"]
+            door = KitDoor(utdK1, utdK2, width, height)
+            kit.doors.append(door)
+
+        for component_json in kit_json["components"]:
+            name = component_json["name"]
+            component_identifier = component_json["id"]
+
+            image = QImage("{}/{}/{}.png".format(kits_path, kit_identifier, component_identifier)).mirrored()
+
+            bwm = read_bwm("{}/{}/{}.wok".format(kits_path, kit_identifier, component_identifier))
+            mdl = BinaryReader.load_file("{}/{}/{}.mdl".format(kits_path, kit_identifier, component_identifier))
+            mdx = BinaryReader.load_file("{}/{}/{}.mdx".format(kits_path, kit_identifier, component_identifier))
+            component = KitComponent(kit, name, image, bwm, mdl, mdx)
+
+            for hook_json in component_json["doorhooks"]:
+                position = Vector3(hook_json["x"], hook_json["y"], hook_json["z"])
+                rotation = hook_json["rotation"]
+                door = kit.doors[hook_json["door"]]
+                edge = hook_json["edge"]
+                hook = KitComponentHook(position, rotation, edge, door)
+                component.hooks.append(hook)
+
+            kit.components.append(component)
+
+        kits.append(kit)
+
+    return kits
