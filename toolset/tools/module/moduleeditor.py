fb3f680c67bedcff9fc4a44a38d64c4f31770620
Nicholas Hugi <hugi.nicholas@hotmail.com>
2022-03-21 00:05:48 +0800

    Load list of resources into tree

--- toolset/tools/module/moduleeditor.py
+++ toolset/tools/module/moduleeditor.py
@@ -1,5 +1,9 @@
-from PyQt5.QtWidgets import QMainWindow, QWidget, QOpenGLWidget
-from pykotor.common.module import Module
+from PyQt5 import QtCore
+from PyQt5.QtCore import QPoint
+from PyQt5.QtWidgets import QMainWindow, QWidget, QOpenGLWidget, QTreeWidgetItem, QMenu, QAction
+from pykotor.common.module import Module, ModuleResource
+from pykotor.common.stream import BinaryWriter
+from pykotor.resource.type import ResourceType
 
 from data.installation import HTInstallation
 from tools.module import moduleeditor_ui
@@ -15,13 +19,84 @@ class ModuleEditor(QMainWindow):
         self.ui = moduleeditor_ui.Ui_MainWindow()
         self.ui.setupUi(self)
         self._setupSignals()
-        self._setupResourceTree()
+        self.rebuildResourceTree()
 
     def _setupSignals(self) -> None:
-        ...
+        self.ui.resourceTree.customContextMenuRequested.connect(self.onResourceTreeContextMenu)
 
-    def _setupResourceTree(self) -> None:
-        ...
+    def rebuildResourceTree(self) -> None:
+        self.ui.resourceTree.clear()
+        categories = {
+            ResourceType.UTC: QTreeWidgetItem(["Creatures"]),
+            ResourceType.UTP: QTreeWidgetItem(["Placeables"]),
+            ResourceType.UTD: QTreeWidgetItem(["Doors"]),
+            ResourceType.UTI: QTreeWidgetItem(["Items"]),
+            ResourceType.UTE: QTreeWidgetItem(["Encounters"]),
+            ResourceType.UTT: QTreeWidgetItem(["Triggers"]),
+            ResourceType.UTW: QTreeWidgetItem(["Waypoints"]),
+            ResourceType.UTS: QTreeWidgetItem(["Sounds"]),
+            ResourceType.UTM: QTreeWidgetItem(["Merchants"]),
+            ResourceType.DLG: QTreeWidgetItem(["Dialogs"]),
+            ResourceType.FAC: QTreeWidgetItem(["Factions"]),
+            ResourceType.MDL: QTreeWidgetItem(["Models"]),
+            ResourceType.TGA: QTreeWidgetItem(["Textures"]),
+            ResourceType.NCS: QTreeWidgetItem(["Scripts"]),
+            ResourceType.IFO: QTreeWidgetItem(["Module Data"]),
+            ResourceType.INVALID: QTreeWidgetItem(["Other"])
+        }
+        categories[ResourceType.MDX] = categories[ResourceType.MDL]
+        categories[ResourceType.WOK] = categories[ResourceType.MDL]
+        categories[ResourceType.TPC] = categories[ResourceType.TGA]
+        categories[ResourceType.IFO] = categories[ResourceType.IFO]
+        categories[ResourceType.ARE] = categories[ResourceType.IFO]
+        categories[ResourceType.GIT] = categories[ResourceType.IFO]
+        categories[ResourceType.LYT] = categories[ResourceType.IFO]
+        categories[ResourceType.VIS] = categories[ResourceType.IFO]
+        categories[ResourceType.PTH] = categories[ResourceType.IFO]
+        categories[ResourceType.NSS] = categories[ResourceType.NCS]
+
+        for category in categories:
+            self.ui.resourceTree.addTopLevelItem(categories[category])
+
+        for resource in self._module.resources.values():
+            item = QTreeWidgetItem([resource.resname() + "." + resource.restype().extension])
+            item.setData(0, QtCore.Qt.UserRole, resource)
+            category = categories[resource.restype()] if resource.restype() in categories else categories[ResourceType.INVALID]
+            category.addChild(item)
+
+        self.ui.resourceTree.sortByColumn(0, QtCore.Qt.AscendingOrder)
+        self.ui.resourceTree.setSortingEnabled(True)
+
+    def openModuleResource(self, resource: ModuleResource) -> None:
+        self.parent().openResourceEditor(resource.active(), resource.resname(), resource.restype(), resource.data())
+
+    def copyResourceToOverride(self, resource: ModuleResource) -> None:
+        location = "{}/{}.{}".format(self._installation.override_path(), resource.resname(), resource.restype().extension)
+        BinaryWriter.dump(location, resource.data())
+        resource.add_locations([location])
+        resource.activate(location)
+
+    def onResourceTreeContextMenu(self, point: QPoint) -> None:
+        menu = QMenu(self)
+
+        data = self.ui.resourceTree.currentItem().data(0, QtCore.Qt.UserRole)
+        if isinstance(data, ModuleResource):
+            copyToOverrideAction = QAction("Copy To Override", self)
+            copyToOverrideAction.triggered.connect(lambda _, r=data: self.copyResourceToOverride(r))
+
+            menu.addAction("Edit Active File").triggered.connect(lambda _, r=data: self.openModuleResource(r))
+            menu.addAction(copyToOverrideAction)
+            menu.addSeparator()
+            for location in data.locations():
+                locationAciton = QAction(location, self)
+                locationAciton.triggered.connect(lambda _, l=location: data.activate(l))
+                if location == data.active():
+                    locationAciton.setEnabled(False)
+                if "override" in location.lower():
+                    copyToOverrideAction.setEnabled(False)
+                menu.addAction(locationAciton)
+
+        menu.exec_(self.ui.resourceTree.mapToGlobal(point))
 
 
 class ModuleRenderer(QOpenGLWidget):
