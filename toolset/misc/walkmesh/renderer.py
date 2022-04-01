3ec8e5e01c178fee781dff5999e854437c40fef0
Nicholas Hugi <hugi.nicholas@hotmail.com>
2022-04-01 14:59:54 +0800

    Hide room boundaries

--- toolset/misc/walkmesh/renderer.py
+++ toolset/misc/walkmesh/renderer.py
@@ -54,7 +54,7 @@ class WalkmeshRenderer(QWidget):
         self._walkmeshFaceCache: Optional[List[QPainterPath]] = None
 
         self.highlightOnHover: bool = False
-        self.highlightBoundaries: bool = True
+        self.highlightBoundaries: bool = False
 
         self.hideWalkmeshEdges: bool = False
 
@@ -506,20 +506,21 @@ class WalkmeshRenderer(QWidget):
             painter.setBrush(self.materialColor(face.material))
             painter.drawPath(path)
 
-        for walkmesh in self._walkmeshes:
-            for face in walkmesh.walkable_faces():
-                painter.setPen(QPen(QColor(255, 0, 0), 3/self._camScale))
-                path = QPainterPath()
-                if face.trans1 is not None:
-                    path.moveTo(face.v1.x, face.v1.y)
-                    path.lineTo(face.v2.x, face.v2.y)
-                if face.trans2 is not None:
-                    path.moveTo(face.v2.x, face.v2.y)
-                    path.lineTo(face.v3.x, face.v3.y)
-                if face.trans3 is not None:
-                    path.moveTo(face.v1.x, face.v1.y)
-                    path.lineTo(face.v3.x, face.v3.y)
-                painter.drawPath(path)
+        if self.highlightBoundaries:
+            for walkmesh in self._walkmeshes:
+                for face in walkmesh.walkable_faces():
+                    painter.setPen(QPen(QColor(255, 0, 0), 3/self._camScale))
+                    path = QPainterPath()
+                    if face.trans1 is not None:
+                        path.moveTo(face.v1.x, face.v1.y)
+                        path.lineTo(face.v2.x, face.v2.y)
+                    if face.trans2 is not None:
+                        path.moveTo(face.v2.x, face.v2.y)
+                        path.lineTo(face.v3.x, face.v3.y)
+                    if face.trans3 is not None:
+                        path.moveTo(face.v1.x, face.v1.y)
+                        path.lineTo(face.v3.x, face.v3.y)
+                    painter.drawPath(path)
 
         # Draw the git instances (represented as icons)
         painter.setOpacity(0.6)
