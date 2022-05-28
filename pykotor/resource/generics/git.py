698a33c8945bc23f001cd4fb92894ed4bb5fc3c7
Nicholas Hugi <hugi.nicholas@hotmail.com>
2022-05-28 15:43:04 +0800

    Set tweak colour to None by default

--- pykotor/resource/generics/git.py
+++ pykotor/resource/generics/git.py
@@ -356,7 +356,7 @@ class GITDoor(GITInstance):
         super().__init__(x, y, z)
         self.resref: ResRef = ResRef.from_blank()
         self.bearing: float = 0.0
-        self.tweak_color: Optional[Color] = Color.WHITE
+        self.tweak_color: Optional[Color] = None
         self.linked_to: str = ""
         self.linked_to_flags: GITModuleLink = GITModuleLink.NoLink
         self.linked_to_module: ResRef = ResRef.from_blank()
@@ -482,7 +482,7 @@ class GITPlaceable(GITInstance):
         super().__init__(x, y, z)
         self.resref: ResRef = ResRef.from_blank()
         self.bearing: float = 0.0
-        self.tweak_color: Optional[Color] = Color.WHITE
+        self.tweak_color: Optional[Color] = None
 
     def move(
             self,
