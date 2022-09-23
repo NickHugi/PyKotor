97d123b59b00e907a635fa045ab882d757fd5b49
Nicholas Hugi <hugi.nicholas@hotmail.com>
2022-09-23 17:49:46 +0800

    Update version number (1.5.3)

--- pykotor/resource/formats/mdl/__init__.py
+++ pykotor/resource/formats/mdl/__init__.py
@@ -1,4 +1,4 @@
-from pykotor.resource.formats.mdl.mdl_data import MDL
+from pykotor.resource.formats.mdl.mdl_data import MDL, MDLNode, MDLMesh
 from pykotor.resource.formats.mdl.io_mdl import MDLBinaryReader, MDLBinaryWriter
 from pykotor.resource.formats.mdl.io_mdl_ascii import MDLAsciiReader, MDLAsciiWriter
 from pykotor.resource.formats.mdl.mdl_auto import write_mdl, read_mdl
