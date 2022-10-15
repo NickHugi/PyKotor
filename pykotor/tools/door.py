4f3b5241dbf1a53fc3e642dc69ed885715f5923e
Nicholas Hugi <hugi.nicholas@hotmail.com>
2022-10-15 13:32:55 +0800

    Add methods for extracting model names for placeables and doors

---
+++ pykotor/tools/door.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+from pykotor.resource.generics.utd import UTD
+
+from pykotor.extract.installation import Installation
+from pykotor.resource.formats.twoda import TwoDA
+from pykotor.resource.generics.utp import UTP
+from pykotor.resource.type import ResourceType
+
+
+def get_model(
+        utd: UTD,
+        installation: Installation,
+        *,
+        genericdoors: Optional[TwoDA] = None,
+) -> str:
+    """
+    Returns the model name for the given door.
+
+    If no value is specified for the genericdoor parameters then it will be loaded from the given installation.
+
+    Args:
+        utd: UTD object of the target door.
+        installation: The relevant installation.
+        genericdoors: The genericdoors.2da loaded into a TwoDA object.
+
+    Returns:
+        Returns the model name for the door.
+    """
+    if genericdoors is None:
+        genericdoors = installation.resource("placeables", ResourceType.TwoDA)
+
+    model = genericdoors.get_row(utd.appearance_id).get_string("modelname")
+
+    return model
---
+++ pykotor/tools/placeable.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from pykotor.extract.installation import Installation
+from pykotor.resource.formats.twoda import TwoDA
+from pykotor.resource.generics.utp import UTP
+from pykotor.resource.type import ResourceType
+
+
+def get_model(
+        utp: UTP,
+        installation: Installation,
+        *,
+        placeables: Optional[TwoDA] = None,
+) -> str:
+    """
+    Returns the model name for the given placeable.
+
+    If no value is specified for the placeable parameters then it will be loaded from the given installation.
+
+    Args:
+        utp: UTC object of the target placeable.
+        installation: The relevant installation.
+        placeables: The placeables.2da loaded into a TwoDA object.
+
+    Returns:
+        Returns the model name for the placeable.
+    """
+    if placeables is None:
+        placeables = installation.resource("genericdoors", ResourceType.TwoDA)
+
+    model = placeables.get_row(utp.appearance_id).get_string("modelname")
+
+    return model
