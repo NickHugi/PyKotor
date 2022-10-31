9dbf90682953ccc0c8bbe63c6339d41573994371
Nicholas Hugi <hugi.nicholas@hotmail.com>
2022-10-31 20:41:44 +0800

    Add support for loading 2DA Row additions from changes.ini

--- pykotor/tslpatcher/reader.py
+++ pykotor/tslpatcher/reader.py
@@ -1,5 +1,5 @@
 from configparser import ConfigParser
-from typing import Dict, Optional
+from typing import Dict, Optional, Union, Tuple
 
 from pykotor.resource.formats.tlk import TLK, read_tlk
 from pykotor.tslpatcher.config import PatcherConfig
@@ -60,91 +60,81 @@ class ConfigReader:
 
     def discern_2da(self, key: str, identifier: str, modifiers: Dict[str, str]) -> Modify2DA:
         if key.startswith("ChangeRow"):
-            if "RowIndex" in modifiers:
-                target = Target(TargetType.ROW_INDEX, int(modifiers["RowIndex"]))
-                modifiers.pop("RowIndex")
-            elif "RowLabel" in modifiers:
-                target = Target(TargetType.ROW_LABEL, modifiers["RowLabel"])
-                modifiers.pop("RowLabel")
-            elif "LabelIndex" in modifiers:
-                target = Target(TargetType.LABEL_COLUMN, modifiers["LabelIndex"])
-                modifiers.pop("LabelIndex")
-            else:
-                raise WarningException("No line set to be modified for '{}'.".format(identifier))
-
-            cells = {}
-            store_2da = {}
-            store_tlk = {}
-
-            for modifier, value in modifiers.items():
-                is_store_2da = modifier.startswith("2DAMEMORY")
-                is_store_tlk = modifier.startswith("StrRef")
-                is_row_label = modifier == "RowLabel" or modifier == "NewRowLabel"
-
-                if value.startswith("2DAMEMORY"):
-                    token_id = int(value[9:])
-                    row_value = RowValue2DAMemory(token_id)
-                elif value.startswith("StrRef"):
-                    token_id = int(value[6:])
-                    row_value = RowValueTLKMemory(token_id)
-                elif value == "high()":
-                    row_value = RowValueHigh(None) if modifier == "RowLabel" else RowValueHigh(value)
-                elif value == "RowIndex":
-                    row_value = RowValueRowIndex()
-                elif value == "RowLabel":
-                    row_value = RowValueRowLabel()
-                elif is_store_2da or is_store_tlk:
-                    row_value = RowValueRowCell(value)
-                else:
-                    row_value = RowValueConstant(value)
-
-                if is_store_2da or is_store_tlk:
-                    token_id = int(modifier[9:]) if is_store_2da else int(modifier[6:])
-                    store = store_2da if is_store_2da else store_tlk
-                    store[token_id] = row_value
-                elif is_row_label:
-                    ...
-                else:
-                    cells[modifier] = row_value
-
-            manipulation = ChangeRow2DA(identifier, target, cells, store_2da, store_tlk)
+            target = self.target_2da(identifier, modifiers)
+            cells, store_2da, store_tlk = self.cells_2da(identifier, modifiers)
+            modification = ChangeRow2DA(identifier, target, cells, store_2da, store_tlk)
+        elif key.startswith("AddRow"):
+            exclusive_column = self.exclusive_column_2da(modifiers)
+            row_label = self.row_label_2da(identifier, modifiers)
+            cells, store_2da, store_tlk = self.cells_2da(identifier, modifiers)
+            modification = AddRow2DA(identifier, exclusive_column, row_label, cells, store_2da, store_tlk)
         else:
             raise WarningException()
-        '''elif key.startswith("AddRow"):
-            manipulation = AddRow2DA()
-            manipulation.identifier = identifier
-            manipulation.exclusive_column = modifiers.pop("ExclusiveColumn", None)
-            manipulation.modifiers = modifiers
-        elif key.startswith("CopyRow"):
-            manipulation = CopyRow2DA()
-            manipulation.identifier = identifier
-            manipulation.target = self.target_2da(modifiers)
-            manipulation.exclusive_column = modifiers.pop("ExclusiveColumn", None)
-            manipulation.modifiers = modifiers
-        elif key.startswith("AddColumn"):
-            manipulation = AddColumn2DA()
-            manipulation.identifier = identifier
-            manipulation.header = modifiers.pop("ColumnLabel")
-            manipulation.default = modifiers.pop("DefaultValue")
-            for modifier in modifiers:
-                if modifier.startswith("I"):
-                    manipulation.index_insert[int(modifier[1:])] = modifiers[modifier]
-                elif modifier.startswith("L"):
-                    manipulation.label_insert[modifier[1:]] = modifiers[modifier]
-                elif modifier.startswith("2DAMEMORY"):
-                    memory_index = int(modifier.replace("2DAMEMORY", ""))
-                    manipulation.memory_saves[memory_index] = modifiers[modifier]'''
 
-        return manipulation
+        return modification
 
-    def target_2da(self, modifiers: Dict[str, str]) -> Target:
+    def target_2da(self, identifier: str, modifiers: Dict[str, str]) -> Target:
         if "RowIndex" in modifiers:
-            target = Target(TargetType.ROW_INDEX, int(modifiers.pop("RowIndex")))
+            target = Target(TargetType.ROW_INDEX, int(modifiers["RowIndex"]))
+            modifiers.pop("RowIndex")
         elif "RowLabel" in modifiers:
-            target = Target(TargetType.ROW_LABEL, modifiers.pop("RowLabel"))
+            target = Target(TargetType.ROW_LABEL, modifiers["RowLabel"])
+            modifiers.pop("RowLabel")
         elif "LabelIndex" in modifiers:
-            target = Target(TargetType.LABEL_COLUMN, modifiers.pop("LabelIndex"))
+            target = Target(TargetType.LABEL_COLUMN, modifiers["LabelIndex"])
+            modifiers.pop("LabelIndex")
         else:
-            raise WarningException()
+            raise WarningException("No line set to be modified for '{}'.".format(identifier))
 
         return target
+
+    def exclusive_column_2da(self, modifiers: Dict[str, str]) -> Optional[str]:
+        if "ExclusiveColumn" in modifiers:
+            return modifiers.pop("ExclusiveColumn")
+        return None
+
+    def cells_2da(self, identifier: str, modifiers: Dict[str, str]) -> Tuple:
+        cells = {}
+        store_2da = {}
+        store_tlk = {}
+
+        for modifier, value in modifiers.items():
+            is_store_2da = modifier.startswith("2DAMEMORY")
+            is_store_tlk = modifier.startswith("StrRef")
+            is_row_label = modifier == "RowLabel" or modifier == "NewRowLabel"
+
+            if value.startswith("2DAMEMORY"):
+                token_id = int(value[9:])
+                row_value = RowValue2DAMemory(token_id)
+            elif value.startswith("StrRef"):
+                token_id = int(value[6:])
+                row_value = RowValueTLKMemory(token_id)
+            elif value == "high()":
+                row_value = RowValueHigh(None) if modifier == "RowLabel" else RowValueHigh(value)
+            elif value == "RowIndex":
+                row_value = RowValueRowIndex()
+            elif value == "RowLabel":
+                row_value = RowValueRowLabel()
+            elif is_store_2da or is_store_tlk:
+                row_value = RowValueRowCell(value)
+            else:
+                row_value = RowValueConstant(value)
+
+            if is_store_2da or is_store_tlk:
+                token_id = int(modifier[9:]) if is_store_2da else int(modifier[6:])
+                store = store_2da if is_store_2da else store_tlk
+                store[token_id] = row_value
+            elif is_row_label:
+                ...
+            else:
+                cells[modifier] = row_value
+
+        return cells, store_2da, store_tlk
+
+    def row_label_2da(self, identifier: str, modifiers: Dict[str, str]) -> Optional[str]:
+        if "RowLabel" in modifiers:
+            return modifiers.pop("RowLabel")
+        elif "NewRowLabel" in modifiers:
+            return modifiers.pop("NewRowLabel")
+        else:
+            return None
--- tests/tslpatcher/test_reader.py
+++ tests/tslpatcher/test_reader.py
@@ -4,7 +4,7 @@ from unittest import TestCase
 from pykotor.resource.formats.tlk import TLK
 from pykotor.tslpatcher.config import PatcherConfig
 from pykotor.tslpatcher.mods.twoda import ChangeRow2DA, TargetType, RowValueRowIndex, RowValueRowLabel, RowValueRowCell, \
-    RowValueConstant, RowValue2DAMemory, RowValueTLKMemory
+    RowValueConstant, RowValue2DAMemory, RowValueTLKMemory, AddRow2DA
 from pykotor.tslpatcher.reader import ConfigReader
 
 
@@ -41,6 +41,7 @@ class TestConfigReader(TestCase):
         self.assertEqual(tlk_mod1.text, "Num2")
         self.assertEqual(tlk_mod2.text, "Num1")
 
+    # region 2DA: Change Row
     def test_2da_changerow_identifier(self):
         """Test that identifier is being loaded correctly."""
 
@@ -208,4 +209,203 @@ class TestConfigReader(TestCase):
         cell_0_appearance: RowValue2DAMemory = mod_2da_0.cells["appearance"]
         self.assertIsInstance(cell_0_appearance, RowValue2DAMemory)
         self.assertEqual(5, cell_0_appearance.token_id)
+    # endregion
 
+    # region 2DA: Add Row
+    def test_2da_addrow_identifier(self):
+        """Test that identifier is being loaded correctly."""
+
+        ini_text = \
+            """
+            [2DAList]
+            Table0=test.2da
+
+            [test.2da]
+            AddRow0=add_row_0
+            AddRow1=add_row_1
+
+            [add_row_0]
+            RowIndex=1
+            [add_row_1]
+            RowLabel=1
+            """
+
+        tlk = TLK()
+
+        ini = ConfigParser()
+        ini.optionxform = str
+        ini.read_string(ini_text)
+
+        config = PatcherConfig("", "")
+        ConfigReader(ini, tlk).load(config)
+
+        # noinspection PyTypeChecker
+        mod_0: AddRow2DA = config.patches_2da[0].modifiers.pop(0)
+        self.assertEqual("add_row_0", mod_0.identifier)
+
+        # noinspection PyTypeChecker
+        mod_1: AddRow2DA = config.patches_2da[0].modifiers.pop(0)
+        self.assertEqual("add_row_1", mod_1.identifier)
+
+    def test_2da_addrow_exclusivecolumn(self):
+        """Test that exclusive column property is being loaded correctly."""
+
+        ini_text = \
+            """
+            [2DAList]
+            Table0=test.2da
+
+            [test.2da]
+            AddRow0=add_row_0
+            AddRow1=add_row_1
+
+            [add_row_0]
+            ExclusiveColumn=label
+            [add_row_1]
+            """
+
+        tlk = TLK()
+
+        ini = ConfigParser()
+        ini.optionxform = str
+        ini.read_string(ini_text)
+
+        config = PatcherConfig("", "")
+        ConfigReader(ini, tlk).load(config)
+
+        # noinspection PyTypeChecker
+        mod_0: AddRow2DA = config.patches_2da[0].modifiers.pop(0)
+        self.assertIsInstance(mod_0, AddRow2DA)
+        self.assertEqual("add_row_0", mod_0.identifier)
+        self.assertEqual("label", mod_0.exclusive_column)
+
+        # noinspection PyTypeChecker
+        mod_1: ChangeRow2DA = config.patches_2da[0].modifiers.pop(0)
+        self.assertIsInstance(mod_1, AddRow2DA)
+        self.assertEqual("add_row_1", mod_1.identifier)
+        self.assertIsNone(mod_1.exclusive_column)
+
+    def test_2da_addrow_rowlabel(self):
+        """Test that row label property is being loaded correctly."""
+
+        ini_text = \
+            """
+            [2DAList]
+            Table0=test.2da
+
+            [test.2da]
+            AddRow0=add_row_0
+            AddRow1=add_row_1
+
+            [add_row_0]
+            RowLabel=123
+            [add_row_1]
+            """
+
+        tlk = TLK()
+
+        ini = ConfigParser()
+        ini.optionxform = str
+        ini.read_string(ini_text)
+
+        config = PatcherConfig("", "")
+        ConfigReader(ini, tlk).load(config)
+
+        # noinspection PyTypeChecker
+        mod_0: AddRow2DA = config.patches_2da[0].modifiers.pop(0)
+        self.assertIsInstance(mod_0, AddRow2DA)
+        self.assertEqual("add_row_0", mod_0.identifier)
+        self.assertEqual("123", mod_0.row_label)
+
+        # noinspection PyTypeChecker
+        mod_1: AddRow2DA = config.patches_2da[0].modifiers.pop(0)
+        self.assertIsInstance(mod_1, AddRow2DA)
+        self.assertEqual("add_row_1", mod_1.identifier)
+        self.assertIsNone(mod_1.row_label)
+
+    def test_2da_addrow_store2da(self):
+        """Test that 2DAMEMORY# data will be saved correctly."""
+
+        ini_text = \
+            """
+            [2DAList]
+            Table0=test.2da
+
+            [test.2da]
+            AddRow0=add_row_0
+
+            [add_row_0]
+            2DAMEMORY0=RowIndex
+            2DAMEMORY1=RowLabel
+            2DAMEMORY2=label
+            """
+
+        tlk = TLK()
+
+        ini = ConfigParser()
+        ini.optionxform = str
+        ini.read_string(ini_text)
+
+        config = PatcherConfig("", "")
+        ConfigReader(ini, tlk).load(config)
+
+        # noinspection PyTypeChecker
+        mod_0: ChangeRow2DA = config.patches_2da[0].modifiers.pop(0)
+
+        # noinspection PyTypeChecker
+        store_0a: RowValueRowIndex = mod_0.store_2da[0]
+        self.assertIsInstance(store_0a, RowValueRowIndex)
+
+        # noinspection PyTypeChecker
+        store_0b: RowValueRowLabel = mod_0.store_2da[1]
+        self.assertIsInstance(store_0b, RowValueRowLabel)
+
+        # noinspection PyTypeChecker
+        store_0c: RowValueRowCell = mod_0.store_2da[2]
+        self.assertIsInstance(store_0c, RowValueRowCell)
+        self.assertEqual("label", store_0c.column)
+
+    def test_2da_addrow_cells(self):
+        """Test that cells will be assigned properly correctly."""
+
+        ini_text = \
+            """
+            [2DAList]
+            Table0=test.2da
+
+            [test.2da]
+            AddRow0=add_row_0
+
+            [add_row_0]
+            label=Test123
+            dialog=StrRef4
+            appearance=2DAMEMORY5
+            """
+
+        tlk = TLK()
+
+        ini = ConfigParser()
+        ini.optionxform = str
+        ini.read_string(ini_text)
+
+        config = PatcherConfig("", "")
+        ConfigReader(ini, tlk).load(config)
+
+        # noinspection PyTypeChecker
+        mod_0: AddRow2DA = config.patches_2da[0].modifiers.pop(0)
+
+        # noinspection PyTypeChecker
+        cell_0_label: RowValueConstant = mod_0.cells["label"]
+        self.assertIsInstance(cell_0_label, RowValueConstant)
+        self.assertEqual("Test123", cell_0_label.string)
+
+        # noinspection PyTypeChecker
+        cell_0_dialog: RowValueTLKMemory = mod_0.cells["dialog"]
+        self.assertIsInstance(cell_0_dialog, RowValueTLKMemory)
+        self.assertEqual(4, cell_0_dialog.token_id)
+
+        # noinspection PyTypeChecker
+        cell_0_appearance: RowValue2DAMemory = mod_0.cells["appearance"]
+        self.assertIsInstance(cell_0_appearance, RowValue2DAMemory)
+        self.assertEqual(5, cell_0_appearance.token_id)
+    # endregion Add Row
