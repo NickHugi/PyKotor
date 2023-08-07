NickHugi/PyKotor

commit 4cae6504c3c4b8b317d1582836c5e1b6259d616c

Refactors and optimizations
Lib/Module repack code improvements by caching keys.
Fixed some typing in some methods.

pykotor/resource/formats/ncs/ncs_data.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 from enum import IntEnum, Enum
-from typing import NamedTuple, Optional, List
+from typing import Any, NamedTuple, Optional, List
 
 from pykotor.common.misc import Game
 
@@ -197,7 +197,13 @@ class NCS:
             else:
                 print("{}:\t{} {}".format(i, instruction.ins_type.name.ljust(8), instruction.args))
 
-    def add(self, instruction_type: NCSInstructionType, args: List = None, jump: Optional[NCSInstruction] = None, index: int = None) -> NCSInstruction:
+    def add(
+        self,
+        instruction_type: NCSInstructionType,
+        args: Optional[List[Any]],
+        jump: Optional[NCSInstruction] = None,
+        index: Optional[int] = None
+    ) -> NCSInstruction:
         instruction = NCSInstruction(instruction_type, args, jump)
         self.instructions.insert(index, instruction) if index is not None else self.instructions.append(instruction)
         return instruction
@@ -209,18 +215,37 @@ class NCS:
         return [inst for inst in self.instructions if inst.jump is target]
 
     def optimize(self, optimizers: List[NCSOptimizer]) -> None:
+        """
+        The `optimize` function takes a list of optimizers and applies each optimizer to optimize the
+        current object.
+
+        :param optimizers: A list of NCSOptimizer objects
+        :type optimizers: List[NCSOptimizer]
+        """
         for optimizer in optimizers:
             optimizer.optimize(self)
 
     def merge(self, other: NCS) -> None:
+        """
+        The `merge` function takes another `NCS` object and appends its instructions to the instructions of
+        the current object.
+
+        :param other: The parameter "other" is of type NCS, which is likely a custom class or data structure
+        :type other: NCS
+        """
         self.instructions.extend(other.instructions)
 
 
 class NCSInstruction:
-    def __init__(self, ins_type: NCSInstructionType = NCSInstructionType.NOP, args: List = None, jump: Optional[NCSInstruction] = None):
+    def __init__(
+        self,
+        ins_type: NCSInstructionType = NCSInstructionType.NOP,
+        args: Optional[List[Any]] = None,
+        jump: Optional[NCSInstruction] = None
+    ):
         self.ins_type: NCSInstructionType = ins_type
         self.jump: Optional[NCSInstruction] = jump
-        self.args: List = args if args is not None else []
+        self.args: List[Any] = args if args is not None else []
 
     def __str__(self):
         if self.jump is None:

pykotor/common/misc.py
@@ -13,15 +13,15 @@ T = TypeVar("T")
 
 
 class ResRef:
+    """
+    A string reference to a game resource. ResRefs can be a maximum of 16 characters in length.
+    """
     class InvalidEncodingError(ValueError):
         ...
 
     class ExceedsMaxLengthError(ValueError):
         ...
 
-    """
-    A string reference to a game resource. ResRefs can be a maximum of 16 characters in length.
-    """
 
     def __init__(
             self,
@@ -37,22 +37,18 @@ class ResRef:
 
     def __eq__(
             self,
-            other
+            other: ResRef | str | object
     ):
         """
         A ResRef can be compared to another ResRef or a str.
         """
-        if isinstance(other, ResRef):
-            return other.get().lower() == self.get().lower()
-        elif isinstance(other, str):
-            return other.lower() == self.get().lower()
-        else:
-            return NotImplemented
+        other_value = other.get().lower() if isinstance(other, ResRef) else other.lower() if isinstance(other, str) else None
+        return other_value == self._value.lower() if other_value is not None else NotImplemented
 
     def __repr__(
             self
     ):
-        return "ResRef({})".format(self._value)
+        return f"ResRef({self._value})"
 
     def __str__(
             self
@@ -69,7 +65,7 @@ class ResRef:
         Returns:
             A new ResRef instance.
         """
-        return ResRef("")
+        return cls("")
 
     @classmethod
     def from_path(
@@ -85,7 +81,7 @@ class ResRef:
         Returns:
             A new ResRef instance.
         """
-        return ResRef(os.path.splitext(path)[0].replace('\\', '/').split('/')[-1])
+        return cls(os.path.splitext(os.path.basename(path.replace('\\', '/')))[0])
 
     def set(
             self,
@@ -102,12 +98,15 @@ class ResRef:
         Raises:
             ValueError:
         """
-        if len(text) > 16 and truncate:
-            text = text[:16]
-        elif len(text) > 16 and not truncate:
-            raise ResRef.ExceedsMaxLengthError("ResRef cannot exceed 16 characters.")
-        if len(text) != len(text.encode()):
-            raise ResRef.InvalidEncodingError("ResRef must be in ASCII characters.")
+        if len(text) > 16:
+            if truncate:
+                text = text[:16]
+            else:
+                raise ResRef.ExceedsMaxLengthError(
+                    "ResRef cannot exceed 16 characters.")
+        if len(text.encode()) != len(text):
+            raise ResRef.InvalidEncodingError(
+                "ResRef must be in ASCII characters.")
 
         self._value = text
 

pykotor/common/module.py
@@ -136,6 +136,15 @@ class Module:
         for location in self.git().locations():
             self.git().activate(location)
             self.git().resource()
+            git = self.git().resource()
+            [look_for.append(ResourceIdentifier(creature.resref.get(), ResourceType.UTC)) for creature in git.creatures]
+            [look_for.append(ResourceIdentifier(placeable.resref.get(), ResourceType.UTP)) for placeable in git.placeables]
+            [look_for.append(ResourceIdentifier(door.resref.get(), ResourceType.UTD)) for door in git.doors]
+            [look_for.append(ResourceIdentifier(sound.resref.get(), ResourceType.UTS)) for sound in git.sounds]
+            [look_for.append(ResourceIdentifier(waypoint.resref.get(), ResourceType.UTW)) for waypoint in git.waypoints]
+            [look_for.append(ResourceIdentifier(encounter.resref.get(), ResourceType.UTE)) for encounter in git.encounters]
+            [look_for.append(ResourceIdentifier(trigger.resref.get(), ResourceType.UTT)) for trigger in git.triggers]
+            [look_for.append(ResourceIdentifier(store.resref.get(), ResourceType.UTM)) for store in git.stores]
         self.git().activate(original)
 
         # Models referenced in LYTs

pykotor/common/stream.py
@@ -13,7 +13,7 @@ from pykotor.common.language import LocalizedString
 
 
 def _endian_char(
-        big
+        big: bool
 ) -> str:
     """
     Returns the character that represents either big endian or small endian in struct unpack.
@@ -46,7 +46,7 @@ class BinaryReader:
             self,
             stream: BinaryIO,
             offset: int = 0,
-            size: int = None
+            size: Optional[int] = None
     ):
         self._stream: BinaryIO = stream
         self._offset: int = offset
@@ -116,7 +116,7 @@ class BinaryReader:
     @classmethod
     def from_auto(
             cls,
-            source: Optional[str, bytes, bytearray, BinaryReader],
+            source: Optional[Union[str, bytes, bytearray, BinaryReader]],
             offset: int = 0,
             size: int = None
     ):
@@ -150,10 +150,7 @@ class BinaryReader:
         """
         with open(path, 'rb') as reader:
             reader.seek(offset)
-            if size == -1:
-                return reader.read()
-            else:
-                return reader.read(size)
+            return reader.read() if size == -1 else reader.read(size)
 
     def offset(
             self
@@ -214,7 +211,7 @@ class BinaryReader:
 
     def skip(
             self,
-            length
+            length: int
     ) -> None:
         """
         Skips ahead in the stream the specified number of bytes.
@@ -238,7 +235,7 @@ class BinaryReader:
 
     def seek(
             self,
-            position
+            position: int
     ) -> None:
         """
         Moves the stream pointer to the byte offset.
@@ -602,7 +599,7 @@ class BinaryReader:
             IOError: If the given number sex exceeds the number of remaining bytes.
         """
         if self.position() + num > self.size():
-            raise IOError("This operation would exceeed the streams boundaries.")
+            raise IOError("This operation would exceed the streams boundaries.")
 
 
 class BinaryWriter(ABC):
@@ -1080,7 +1077,7 @@ class BinaryWriterFile(BinaryWriter):
 
     def seek(
             self,
-            position
+            position: int
     ) -> None:
         """
         Moves the stream pointer to the byte offset.
@@ -1296,9 +1293,7 @@ class BinaryWriterFile(BinaryWriter):
             value: The value to be written.
             big: Write bytes as big endian.
         """
-        self._stream.write(struct.pack(_endian_char(big) + 'f', value.x))
-        self._stream.write(struct.pack(_endian_char(big) + 'f', value.y))
-        self._stream.write(struct.pack(_endian_char(big) + 'f', value.z))
+        self._extracted_from_write_vector4_14(big, value)
 
     def write_vector4(
             self,
@@ -1313,10 +1308,14 @@ class BinaryWriterFile(BinaryWriter):
             value: The value to be written.
             big: Write bytes as big endian.
         """
+        self._extracted_from_write_vector4_14(big, value)
+        self._stream.write(struct.pack(_endian_char(big) + 'f', value.w))
+
+    # TODO Rename this here and in `write_vector3` and `write_vector4`
+    def _extracted_from_write_vector4_14(self, big, value):
         self._stream.write(struct.pack(_endian_char(big) + 'f', value.x))
         self._stream.write(struct.pack(_endian_char(big) + 'f', value.y))
         self._stream.write(struct.pack(_endian_char(big) + 'f', value.z))
-        self._stream.write(struct.pack(_endian_char(big) + 'f', value.w))
 
     def write_bytes(
             self,
@@ -1432,7 +1431,7 @@ class BinaryWriterBytearray(BinaryWriter):
     ):
         self._ba = ba
         self._offset: int = offset
-        self._position = 0
+        self._position: int = 0
 
     def __enter__(
             self

pykotor/extract/file.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
 import os
-from typing import Optional, NamedTuple
+from typing import Any, Optional, NamedTuple, Union
 
 from pykotor.resource.type import ResourceType
-from pykotor.tools.misc import is_bif_file, is_erf_file, is_mod_file, is_rim_file
+from pykotor.tools.misc import is_bif_file, is_capsule_file
 
 
 class FileResource:
@@ -38,7 +38,7 @@ class FileResource:
 
     def __eq__(
             self,
-            other: FileResource
+            other: Union[FileResource, ResourceIdentifier]
     ):
         if isinstance(other, FileResource):
             return other._resname.lower() == self._resname.lower() and other._restype == self._restype
@@ -84,7 +84,7 @@ class FileResource:
             Bytes data of the resource.
         """
         if reload:
-            if is_mod_file(self._filepath) or is_erf_file(self._filepath) or is_rim_file(self._filepath):
+            if is_capsule_file(self._filepath):
                 from pykotor.extract.capsule import Capsule
                 capsule = Capsule(self._filepath)
                 res = capsule.info(self._resname, self._restype)

pykotor/extract/talktable.py
@@ -44,20 +44,10 @@ class TalkTable:
         if stringref == -1 or stringref >= entries_count:
             string = ""
         else:
-            reader.seek(20 + 40 * stringref)
-            flags = reader.read_uint32()
-            sound_resref = reader.read_string(16)
-            volume_variance = reader.read_uint32()
-            pitch_variance = reader.read_uint32()
-            text_offset = reader.read_uint32()
-            text_length = reader.read_uint32()
-            sound_length = reader.read_single()
-
+            _, _, _, _, text_offset, text_length, _ = self._extract_common_data(reader, stringref)
             reader.seek(texts_offset + text_offset)
             string = reader.read_string(text_length)
-
         reader.close()
-
         return string
 
     def sound(
@@ -76,23 +66,26 @@ class TalkTable:
         reader = BinaryReader.from_file(self._path)
         reader.seek(12)
         entries_count = reader.read_uint32()
-        texts_offset = reader.read_uint32()
+        _ = reader.read_uint32() # Unused texts_offset
 
         if stringref == -1 or stringref >= entries_count:
             sound_resref = ""
         else:
-            reader.seek(20 + 40 * stringref)
-            flags = reader.read_uint32()
-            sound_resref = reader.read_string(16)
-            volume_variance = reader.read_uint32()
-            pitch_variance = reader.read_uint32()
-            text_offset = reader.read_uint32()
-            text_length = reader.read_uint32()
-            sound_length = reader.read_single()
-
+            _, sound_resref, _, _, _, _, _ = self._extract_common_data(reader, stringref)
         reader.close()
-
         return ResRef(sound_resref)
+
+    def _extract_common_data(self, reader: BinaryReader, stringref: int):
+        reader.seek(20 + 40 * stringref)
+        flags = reader.read_uint32()
+        sound_resref = reader.read_string(16)
+        volume_variance = reader.read_uint32()
+        pitch_variance = reader.read_uint32()
+        text_offset = reader.read_uint32()
+        text_length = reader.read_uint32()
+        sound_length = reader.read_single()
+
+        return flags, sound_resref, volume_variance, pitch_variance, text_offset, text_length, sound_length
 
     def batch(
             self,
@@ -113,21 +106,14 @@ class TalkTable:
         entries_count = reader.read_uint32()
         texts_offset = reader.read_uint32()
 
-        batch = {}
+        batch: Dict[int, StringResult] = {}
 
         for stringref in stringrefs:
             if stringref == -1 or stringref >= entries_count:
                 batch[stringref] = StringResult("", ResRef.from_blank())
                 continue
 
-            reader.seek(20 + 40 * stringref)
-            flags = reader.read_uint32()
-            sound_resref = reader.read_string(16)
-            volume_variance = reader.read_uint32()
-            pitch_variance = reader.read_uint32()
-            text_offset = reader.read_uint32()
-            text_length = reader.read_uint32()
-            sound_length = reader.read_single()
+            _, sound_resref, _, _, text_offset, text_length, _ = self._extract_common_data(reader, stringref)
 
             reader.seek(texts_offset + text_offset)
             string = reader.read_string(text_length)

commit 95b18b94ffd994df479602dd24a100dea0daa438

NCS's `add` args should always be optional (broken in 4cae6504c3c4b8b317d1582836c5e1b6259d616c)

pykotor/resource/formats/ncs/ncs_data.py
@@ -200,7 +200,7 @@ class NCS:
     def add(
         self,
         instruction_type: NCSInstructionType,
-        args: Optional[List[Any]],
+        args: Optional[List[Any]] = None,
         jump: Optional[NCSInstruction] = None,
         index: Optional[int] = None
     ) -> NCSInstruction: