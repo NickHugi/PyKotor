a7c9c9b6e9af3df42c541c6f73c38c6ff55bad23
Benjamin Auquite <halomastar@gmail.com>
2023-09-07 01:56:50 -0500

    see last

--- pykotor/resource/formats/bwm/bwm_auto.py
+++ pykotor/resource/formats/bwm/bwm_auto.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from pykotor.resource.formats.bwm import BWM, BWMBinaryReader, BWMBinaryWriter
 from pykotor.resource.type import SOURCE_TYPES, TARGET_TYPES, ResourceType
 
