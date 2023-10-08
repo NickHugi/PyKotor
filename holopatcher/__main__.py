06ebf7506a1214d8644abd3acbfc2f1fa3514a14
Benjamin Auquite <halomastar@gmail.com>
2023-10-07 20:35:25 -0500

    refactor holopatcher

--- holopatcher/__main__.py
+++ holopatcher/__main__.py
@@ -1,23 +1,43 @@
+from __future__ import annotations
+
 import os
+import sys
+import tkinter as tk
 from configparser import ConfigParser
 from threading import Thread
-from tkinter import *
-from tkinter import ttk
-from tkinter import filedialog
-from typing import List
-from tkinter.messagebox import showerror, showwarning, showinfo
+from tkinter import filedialog, messagebox, ttk
+
+
+def is_debugging() -> bool:
+    # Check if being debugged in popular IDEs. From stackoverflow.
+    return any(
+        [
+            bool(getattr(sys, "_MEIPASS", False)),
+            bool(getattr(sys, "gettrace", None)),
+            "__pydevd__" in sys.modules,
+            "pydevconsole" in sys.modules,
+            "vscodedebugpy" in sys.modules,
+        ],
+    )
+
+
+# Append parent paths for debugging support.
+if is_debugging():
+    sys.path.append(".")
+    sys.path.append("..")
 
 from pykotor.tools.path import CaseAwarePath
-from pykotor.tslpatcher.config import PatcherNamespace, ModInstaller
+from pykotor.tslpatcher.config import ModInstaller, PatcherNamespace
 from pykotor.tslpatcher.logger import PatchLogger
 from pykotor.tslpatcher.reader import NamespaceReader
 
 
-class App(Tk):
+class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.geometry("400x500")
-        self.resizable(False, False)
+        self.resizable(width=False, height=False)
+        self.title("HoloPatcher")
 
         self.mod_path = ""
         self.namespaces = []
@@ -34,7 +54,7 @@ class App(Tk):
         self.gamepath_entry = ttk.Entry(self)
         self.gamepath_entry.place(x=5, y=35, width=310, height=25)
 
-        self.description_text = Text(self, state="disabled", wrap="none")
+        self.description_text = tk.Text(self, state="disabled", wrap="none")
         self.description_text.place(x=5, y=65, width=390, height=400)
 
         ttk.Button(self, text="Browse", command=self.open_mod).place(x=320, y=5, width=75, height=25)
@@ -42,48 +62,63 @@ class App(Tk):
         ttk.Button(self, text="Install", command=self.begin_install).place(x=5, y=470, width=75, height=25)
         ttk.Progressbar(self).place(x=85, y=470, width=310, height=25)
 
-    def open_mod(self) -> None:
-        directory = CaseAwarePath(filedialog.askdirectory())
-        self.mod_path = directory
+        self.open_mod(CaseAwarePath.cwd())
 
-        namespace_path = directory / "tslpatchdata" / "namespaces.ini"
-        changes_path = directory / "tslpatchdata" / "changes.ini"
+    def open_mod(self, default_directory_path_str: os.PathLike | str | None = None) -> None:
+        directory_path_str = default_directory_path_str or filedialog.askdirectory()
+        self.mod_path = directory_path_str
+
+        namespace_path = CaseAwarePath(directory_path_str, "tslpatchdata", "namespaces.ini")
+        changes_path = CaseAwarePath(directory_path_str, "tslpatchdata", "changes.ini")
 
         try:
-            if os.path.exists(namespace_path):
+            if namespace_path.exists():
                 namespaces = NamespaceReader.from_filepath(namespace_path)
                 self.load_namespace(namespaces)
-            elif os.path.exists(changes_path):
+            elif changes_path.exists():
                 namespaces = self.build_changes_as_namespace(changes_path)
                 self.load_namespace([namespaces])
-            else:
-                showerror("Error", "Could not find a mod located at the given folder.")
+            elif not default_directory_path_str:
+                messagebox.showerror("Error", "Could not find a mod located at the given folder.")
         except Exception as e:
-            showerror("Error", "An unknown error occured loading the mod info.")
+            messagebox.showerror("Error", f"An unexpected error occurred while loading mod info: {e}")
 
     def open_kotor(self) -> None:
         directory = CaseAwarePath(filedialog.askdirectory())
-        self.gamepath_entry.delete(0, END)
-        self.gamepath_entry.insert(0, directory)
+
+        # handle possibility of user selecting a nested folder within KOTOR dir (usually override)
+        # TODO: double check if chitin.key exists on mac/mobile versions.
+        while (
+            directory.parent.name
+            and directory.parent.name != directory.parts[1]
+            and not directory.joinpath("chitin.key").exists()
+        ):
+            directory = directory.parent
+        self.gamepath_entry.delete(0, tk.END)
+        self.gamepath_entry.insert(0, str(directory))
 
     def begin_install(self) -> None:
         try:
             Thread(target=self.begin_install_thread).start()
         except Exception as e:
-            showerror("Error", "An unknown error occured during the installation and the program was forced to exit.")
+            messagebox.showerror(
+                "Error",
+                f"An unexpected error occurred during the installation and the program was forced to exit: {e}",
+            )
 
     def begin_install_thread(self):
-        mod_path = CaseAwarePath(self.mod_path) / "tslpatchdata"
-        game_path = CaseAwarePath(self.gamepath_entry.get())
-        ini_file = [x for x in self.namespaces if x.name == self.namespaces_combobox.get()][0].ini_filename
+        mod_path = CaseAwarePath(self.mod_path, "tslpatchdata")
+        game_path = self.gamepath_entry.get()
+        ini_file = next(x for x in self.namespaces if x.name == self.namespaces_combobox.get()).ini_filename
 
-        self.description_text.delete(0, END)
+        self.description_text.delete(0, tk.END)
 
         installer = ModInstaller(mod_path, game_path, ini_file, self.logger)
         installer.install()
 
-    def build_changes_as_namespace(self, filepath: os.PathLike) -> PatcherNamespace:
-        with open(filepath, 'r') as file:
+    def build_changes_as_namespace(self, filepath: os.PathLike | str) -> PatcherNamespace:
+        c_filepath = CaseAwarePath(filepath)
+        with c_filepath.open() as file:
             ini = ConfigParser(
                 delimiters=("="),
                 allow_no_value=True,
@@ -100,15 +135,15 @@ class App(Tk):
 
         return namespace
 
-    def load_namespace(self, namespaces: List[PatcherNamespace]) -> None:
-        self.namespaces_combobox['values'] = namespaces
-        self.namespaces_combobox.set(self.namespaces_combobox['values'][0])
+    def load_namespace(self, namespaces: list[PatcherNamespace]) -> None:
+        self.namespaces_combobox["values"] = namespaces
+        self.namespaces_combobox.set(self.namespaces_combobox["values"][0])
         self.namespaces = namespaces
 
     def write_log(self, message: str) -> None:
         self.description_text.config(state="normal")
-        self.description_text.insert(END, message + "\n")
-        self.description_text.see(END)
+        self.description_text.insert(tk.END, message + os.linesep)
+        self.description_text.see(tk.END)
         self.description_text.config(state="disabled")
 
 
