25ed937c4576efa225dd29b897c01220e84614c5
Benjamin Auquite <halomastar@gmail.com>
2023-10-13 17:26:11 -0500

    Update nss.py

--- pykotor/tslpatcher/mods/nss.py
+++ pykotor/tslpatcher/mods/nss.py
@@ -20,15 +20,15 @@ class ModificationsNSS:
         match = re.search(r"#2DAMEMORY\d+#", source)
         while match:
             token_id = int(source[match.start() + 10 : match.end() - 1])
-            value_str: str = memory.memory_2da[token_id]
-            source = source[: match.start()] + value_str + source[match.end() :]
+            value = memory.memory_2da[token_id]
+            source = source[0 : match.start()] + str(value) + source[match.end() : len(source)]
             match = re.search(r"#2DAMEMORY\d+#", source)
 
         match = re.search(r"#StrRef\d+#", source)
         while match:
             token_id = int(source[match.start() + 7 : match.end() - 1])
-            value: int = memory.memory_str[token_id]
-            source = source[: match.start()] + str(value) + source[match.end() :]
+            value = memory.memory_str[token_id]
+            source = source[0 : match.start()] + str(value) + source[match.end() : len(source)]
             match = re.search(r"#StrRef\d+#", source)
 
         nss[0] = source
