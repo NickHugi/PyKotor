8d893fd9fd0d75d1ea0ebcdd7d48ab9e189735b7
Benjamin Auquite <halomastar@gmail.com>
2023-11-06 23:02:35 -0600

    Update template.py

--- pykotor/tslpatcher/mods/template.py
+++ pykotor/tslpatcher/mods/template.py
@@ -27,7 +27,7 @@ class PatcherModifications(ABC):
         self.destination: str = self.DEFAULT_DESTINATION
 
         self.action: str = "Patch" + " "
-        self.override_type: str = OverrideType.IGNORE
+        self.override_type: str = OverrideType.WARN
         self.skip_if_not_replace = False  # [InstallList] only?
 
     @abstractmethod
