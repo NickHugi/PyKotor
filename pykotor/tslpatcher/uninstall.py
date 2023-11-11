b3786e3a08ed8325d468826f72ad492e14efb3d5
Benjamin Auquite <halomastar@gmail.com>
2023-11-11 17:58:23 -0600

    general fixes

--- pykotor/tslpatcher/uninstall.py
+++ pykotor/tslpatcher/uninstall.py
@@ -1,7 +1,6 @@
 from pykotor.common.misc import Game
 from pykotor.extract.installation import Installation
-from pykotor.resource.formats.tlk import read_tlk
-from pykotor.resource.formats.tlk.tlk_auto import write_tlk
+from pykotor.resource.formats.tlk import read_tlk, write_tlk
 from pykotor.tools.misc import is_mod_file
 from pykotor.tools.path import CaseAwarePath
 
@@ -22,7 +21,7 @@ def uninstall_all_mods(installation: Installation):
     dialog_tlk_path = CaseAwarePath(root_path, "dialog.tlk")
     dialog_tlk = read_tlk(dialog_tlk_path)
     dialog_tlk.entries = dialog_tlk.entries[:49265] if installation.game() == Game.K1 else dialog_tlk.entries[:136329]
-    # TODO: With the new ReplaceTLK syntax, the above TLK reinstall isn't possible anymore.
+    # TODO: With the new Replace TLK syntax, the above TLK reinstall isn't possible anymore.
     # Here, we should write the dialog.tlk and then check it's sha1 hash compared to vanilla.
     # We could keep the vanilla TLK entries in a tlkdefs.py file, similar to our nwscript.nss defs in scriptdefs.py.
     # This implementation would be required regardless in K2 anyway as this function currently isn't determining if the Aspyr patch and/or TSLRCM is installed.
