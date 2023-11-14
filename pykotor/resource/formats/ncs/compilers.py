80146897ce00289e4a8415f37de2cbddedb7edc7
Benjamin Auquite <halomastar@gmail.com>
2023-11-14 02:10:17 -0600

    add support for the other nwnnsscomp.exe versions

--- pykotor/resource/formats/ncs/compilers.py
+++ pykotor/resource/formats/ncs/compilers.py
@@ -1,7 +1,12 @@
+from __future__ import annotations
+import os
+
 import subprocess
 
 from pykotor.common.misc import Game
 from pykotor.common.stream import BinaryReader
+from pykotor.helpers.misc import generate_filehash_sha256
+from pykotor.helpers.path import Path
 from pykotor.resource.formats.ncs.ncs_auto import compile_nss, write_ncs
 from pykotor.resource.formats.ncs.ncs_data import NCSCompiler
 
@@ -13,23 +18,59 @@ class InbuiltNCSCompiler(NCSCompiler):
         write_ncs(ncs, output_path)
 
 
-class ExternalNCSCompiler(NCSCompiler):
-    def __init__(self, nwnnsscomp_path: str):
-        self.nwnnsscomp_path: str = nwnnsscomp_path
+class ExternalNCSCompiler(NCSCompiler):  # TODO: This currently uses the nwnnsscomp.exe modified for TSLPatcher. Needs to support the other versions as well.
+    def __init__(self, nwnnsscomp_path: os.PathLike | str):
+        self.nwnnsscomp_path: Path = nwnnsscomp_path if isinstance(nwnnsscomp_path, Path) else Path(nwnnsscomp_path)
+        self.filehash: str | None = None
 
-    def compile_script(self, source_filepath: str, output_filepath: str, game: Game) -> None:
-        subprocess.call(
-            [
-                self.nwnnsscomp_path,
-                "-c",
-                source_filepath,
-                "-o",
-                output_filepath,
-                #"-g",
-                #str(game.value),
-            ],
-            stdin=None,
-            stdout=None,
-            stderr=None,
-            shell=False,
-        )
+    def calculate_filehash(self):
+        self.filehash = generate_filehash_sha256(self.nwnnsscomp_path)
+
+    def compile_script(self, source_file: os.PathLike | str, output_file: os.PathLike | str, game: Game) -> None:
+        source_filepath = source_file if isinstance(source_file, Path) else Path(source_file)
+        output_filepath = output_file if isinstance(output_file, Path) else Path(output_file)
+
+        if not self.filehash:
+            self.calculate_filehash()
+        if not self.filehash:
+            msg = "Filehash not calculated"
+            raise ValueError(msg)
+
+        if self.filehash.upper() == "E36AA3172173B654AE20379888EDDC9CF45C62FBEB7AB05061C57B52961C824D":  # KTool
+            subprocess.call(
+                args=[
+                    str(source_filepath),
+                    "--outputdir",
+                    str(output_filepath.parent),
+                    "-o",
+                    output_filepath.name,
+                    "-g",
+                    str(game.value),
+                    "--optimize",
+                ],
+                executable=str(self.nwnnsscomp_path),
+                timeout=15,
+            )
+        elif self.filehash.upper() == "EC3E657C18A32AD13D28DA0AA3A77911B32D9661EA83CF0D9BCE02E1C4D8499D":  # v1
+            subprocess.call(
+                args=[
+                    "-c",
+                    "-o",
+                    str(source_filepath),
+                    str(output_filepath),
+                ],
+                executable=str(self.nwnnsscomp_path),
+                timeout=15,
+            )
+        elif self.filehash.upper() == "539EB689D2E0D3751AEED273385865278BEF6696C46BC0CAB116B40C3B2FE820":  # TSLPatcher
+            subprocess.call(
+                args=[
+                    "-c",
+                    str(source_filepath),
+                    "-o",
+                    str(output_filepath),
+                ],
+                executable=str(self.nwnnsscomp_path),
+                cwd=str(self.nwnnsscomp_path.parent),
+                timeout=15,
+            )
