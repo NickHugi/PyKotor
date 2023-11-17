c23b6dd00bbe0ef3efbe5e0bc46521a1a1465d24
Benjamin Auquite <halomastar@gmail.com>
2023-11-16 23:01:49 -0600

    fix mdl editor

--- toolset/gui/editors/mdl.py
+++ toolset/gui/editors/mdl.py
@@ -84,8 +84,8 @@ class MDLEditor(Editor):
 
         if restype == ResourceType.MDL:
             mdl_data = data
-            if c_filepath.suffix.lower == ".mdl":
-                mdx_data = BinaryReader.load_file(str(c_filepath.with_suffix(".mdx")))
+            if c_filepath.endswith(".mdl"):
+                mdx_data = BinaryReader.load_file(c_filepath.with_suffix(".mdx"))
             elif is_erf_or_mod_file(c_filepath.name):
                 erf = read_erf(filepath)
                 mdx_data = erf.get(resref, ResourceType.MDX)
@@ -97,9 +97,7 @@ class MDLEditor(Editor):
         elif restype == ResourceType.MDX:
             mdx_data = data
             if c_filepath.endswith(".mdx"):
-                mdl_data = BinaryReader.load_file(
-                    str(c_filepath.with_suffix(c_filepath.suffix.lower().replace(".mdl", ".mdx"))),
-                )
+                mdl_data = BinaryReader.load_file(c_filepath.with_suffix(".mdx"))
             elif is_erf_or_mod_file(c_filepath.name):
                 erf = read_erf(filepath)
                 mdl_data = erf.get(resref, ResourceType.MDX)
