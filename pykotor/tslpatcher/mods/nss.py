7147502ffde5c2d30ac423a57c5f1172564839e1
Benjamin Auquite <halomastar@gmail.com>
2023-11-19 06:36:52 -0600

    patcher - don't require nwnnsscomp.exe

--- pykotor/tslpatcher/mods/nss.py
+++ pykotor/tslpatcher/mods/nss.py
@@ -1,17 +1,18 @@
 from __future__ import annotations
 
+import contextlib
 import os
 import re
 from tempfile import TemporaryDirectory
 from typing import TYPE_CHECKING
 
-from pykotor.tools.encoding import decode_bytes_with_fallbacks
 from pykotor.common.stream import BinaryReader
-from pykotor.utility.path import Path, PurePath
 from pykotor.resource.formats.ncs import bytes_ncs
 from pykotor.resource.formats.ncs import compile_nss as compile_with_builtin
 from pykotor.resource.formats.ncs.compilers import ExternalNCSCompiler
+from pykotor.tools.encoding import decode_bytes_with_fallbacks
 from pykotor.tslpatcher.mods.template import PatcherModifications
+from pykotor.utility.path import Path, PurePath
 
 if TYPE_CHECKING:
     from pykotor.common.misc import Game
@@ -27,33 +28,39 @@ class ModificationsNSS(PatcherModifications):
         self.action: str = "Compile"
         self.nwnnsscomp_path: Path
 
+    @staticmethod
+    def load(nss_source: SOURCE_TYPES) -> bytes | None:
+        if isinstance(nss_source, bytearray):
+            return bytes(nss_source)
+        if isinstance(nss_source, bytes):
+            return nss_source
+        if isinstance(nss_source, (os.PathLike, str)):
+            return BinaryReader.load_file(nss_source)
+        return None
+
     def apply(self, nss_source: SOURCE_TYPES, memory: PatcherMemory, logger: PatchLogger, game: Game) -> bytes:
-        """Takes the source nss bytes and replaces instances of 2DAMEMORY# and StrRef# with the relevant data.
+        """Takes the source nss bytes and replaces instances of 2DAMEMORY# and StrRef# with the values in patcher memory. Compiles the
+        source bytes and returns the ncs compiled script as a bytes object.
 
         Args:
         ----
-            nss_source (SOURCE_TYPES): The source script to patch. (path or bytes object)
-            memory (PatcherMemory): Memory references for patching.
-            logger (PatchLogger): Logger for logging messages.
-            game (Game IntEnum): The game being patched.
+            nss_source: SOURCE_TYPES: NSS source object to apply modifications to
+            memory: (PatcherMemory): current memory of 2damemory and strref
+            logger (PatchLogger): Logging object
+            game (Game): KOTOR Game enum value
 
         Returns:
         -------
-            bytes: The patched bytes.
+            bytes: Compiled NCS bytes
+
         Processing Logic:
-            1. Decodes bytes to a string
-            2. Replaces #2DAMEMORY# tokens with values from PatcherMemory
-            3. Replaces #StrRef# tokens with values from PatcherMemory
-            4. Compiles the patched string and encodes to bytes.
+        1. Loads NSS source bytes and decodes
+        2. Replaces #2DAMEMORY and StrRef# tokens with values from patcher memory
+        3. Attempts to compile with external NWN compiler if on Windows
+        4. Falls back to built-in compiler if external isn't available, fails, or not on Windows
         """
-        nss_bytes: bytes
-        if isinstance(nss_source, bytearray):
-            nss_bytes = bytes(nss_source)
-        elif isinstance(nss_source, bytes):
-            nss_bytes = nss_source
-        elif isinstance(nss_source, (os.PathLike, str)):
-            nss_bytes = BinaryReader.load_file(nss_source)
-        else:
+        nss_bytes: bytes | None = self.load(nss_source)
+        if nss_bytes is None:
             logger.add_error(f"Invalid nss source provided to ModificationsNSS.apply(), got {type(nss_source)}")
             return b""
 
@@ -73,8 +80,7 @@ class ModificationsNSS(PatcherModifications):
             source = source[: match.start()] + str(value) + source[match.end() :]
             match = re.search(r"#StrRef\d+#", source)
 
-        if os.name == "nt":
-            source_script = self.nwnnsscomp_path.parent / self.sourcefile
+        if os.name == "nt" and self.nwnnsscomp_path.exists():
             nwnnsscompiler = ExternalNCSCompiler(self.nwnnsscomp_path)
             detected_nwnnsscomp = next((key for key, value in ExternalNCSCompiler.NWNNSSCOMP_SHA256_HASHES.items() if value == nwnnsscompiler.filehash), None)
             if detected_nwnnsscomp != "TSLPatcher":
@@ -83,16 +89,22 @@ class ModificationsNSS(PatcherModifications):
                     f"PyKotor has detected that the provided nwnnsscomp.exe is the '{detected_nwnnsscomp}' version.\n"
                     "PyKotor will compile regardless, but this may not yield the expected result.",
                 )
-            with TemporaryDirectory() as tempdir:
+            with contextlib.suppress(Exception), TemporaryDirectory() as tempdir:
+                source_script = self.nwnnsscomp_path.parent / self.sourcefile
                 tempcompiled_filepath = Path(tempdir, "temp_script.ncs")
                 nwnnsscompiler.compile_script(source_script, tempcompiled_filepath, game)
                 return BinaryReader.load_file(tempcompiled_filepath)
-        if os.name == "posix":
-            # Compile using built-in script compiler.
-            return bytes_ncs(compile_with_builtin(source, game))
 
-        logger.add_error("Operating system not supported - cannot compile script.")
-        return b""
+        if os.name == "nt":
+            if not self.nwnnsscomp_path.exists():
+                logger.add_note("nwnnsscomp.exe was not found in the 'tslpatchdata' folder, using the built-in compilers...")
+            else:
+                logger.add_error(f"An error occurred while compiling {self.sourcefile} with nwnnsscomp.exe, falling back to the built-in compilers...")
+        else:
+            logger.add_note(f"Patching from a unix operating system, compiling '{self.sourcefile}' using the built-in compilers...")
+
+        # Compile using built-in script compiler if external compiler fails.
+        return bytes_ncs(compile_with_builtin(source, game))
 
     def pop_tslpatcher_vars(self, file_section_dict, default_destination=PatcherModifications.DEFAULT_DESTINATION):
         super().pop_tslpatcher_vars(file_section_dict, default_destination)
