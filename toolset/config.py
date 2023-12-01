769c532e702dad901d79fec3fc4e6f63a8c6d480
Benjamin Auquite <halomastar@gmail.com>
2023-12-01 13:03:14 -0600

    fix setup.py, remove duplicates

--- toolset/config.py
+++
@@ -1,2 +0,0 @@
-UPDATE_INFO_LINK = "https://pastebin.com/raw/tUJCGgrX"
-PROGRAM_VERSION = "2.1.1"
--- toolset/globalsettings.py
+++
@@ -1,5 +0,0 @@
-import os
-from typing import List, Dict, Any
-
-from PyQt5.QtCore import QSettings
-
