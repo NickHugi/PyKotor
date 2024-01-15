7bf3bade8ae61c6826f0f62db6e79fc9f8d6c7ac
Benjamin Auquite <halomastar@gmail.com>
2024-01-15 17:54:00 -0600

    cleanup last commit

--- Tools/HoloPatcher/src/__main__.py
+++ Tools/HoloPatcher/src/__main__.py
@@ -850,7 +850,8 @@ class App(tk.Tk):
             7. Shows success or error message based on install result
             8. If CLI, exit regardless of success or error.
         """
-        if installer.config().confirm_message and not messagebox.askokcancel("This mod requires confirmation", installer.config().confirm_message):
+        confirm_msg: str = installer.config().confirm_message.strip()
+        if confirm_msg and not messagebox.askokcancel("This mod requires confirmation", confirm_msg):
             return
         self.set_active_install(install_running=True)
         #profiler = cProfile.Profile()
