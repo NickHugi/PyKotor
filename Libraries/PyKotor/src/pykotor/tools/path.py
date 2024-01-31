7432af5d5d9f55e242239ec83dd7e3014f60d2fa
Benjamin Auquite <halomastar@gmail.com>
2024-01-31 05:34:31 -0600

    oops [skip-edge]

--- Libraries/PyKotor/src/pykotor/tools/path.py
+++ Libraries/PyKotor/src/pykotor/tools/path.py
@@ -10,7 +10,7 @@ from utility.misc import is_instance_or_subinstance
 from utility.system.path import Path as InternalPath
 from utility.system.path import PathElem
 from utility.system.path import PurePath as InternalPurePath
-from utility.system.registry import resolve_reg_key_to_path
+from utility.registry import resolve_reg_key_to_path
 
 if TYPE_CHECKING:
     from pykotor.common.misc import Game
