e473282203606dcba3541c9e488ac0f8688231c9
Benjamin Auquite <halomastar@gmail.com>
2024-02-13 22:04:29 -0600

    fix pacman packages for arch linux

--- compile/compile_toolset.ps1
+++ compile/compile_toolset.ps1
@@ -54,7 +54,7 @@ if ((Get-OS) -eq "Mac") {
             break
         }
         "arch" {
-            $command = "sudo pacman -Syu --noconfirm && sudo pacman -S python-opengl mpdecimal python-pyqt5 qt5-base qt5-multimedia qt5-svg pulseaudio pulseaudio-alsa gstreamer mesa libglvnd ttf-dejavu fontconfig gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly --noconfirm"
+            $command = "sudo pacman -Syu --noconfirm && sudo pacman -S libxcb qt5-base qt5-wayland xcb-util-wm xcb-util-keysyms xcb-util-image xcb-util-renderutil python-opengl libxcomposite gtk3 atk mpdecimal python-pyqt5 qt5-base qt5-multimedia qt5-svg pulseaudio pulseaudio-alsa gstreamer mesa libglvnd ttf-dejavu fontconfig gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly --noconfirm"
             break
         }
     }
