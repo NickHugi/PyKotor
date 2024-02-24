9d9f145b5bf99d6cfd8517ff90979680bc388ea1
Benjamin Auquite <halomastar@gmail.com>
2024-02-24 01:37:25 -0600

    update docs/powershell scripts
    - separate the dependency install code from the compile scripts
    - cleanup the docs on their usage.
    - fix an issue with PyOpenGL_accelerate being installed automatically (this package breaks some environments)

--- compile/compile_batchpatcher.ps1
+++ compile/compile_batchpatcher.ps1
@@ -10,81 +10,14 @@ Write-Host "The path to the script directory is: $scriptPath"
 Write-Host "The path to the root directory is: $rootPath"
 
 Write-Host "Initializing python virtual environment..."
-. $rootPath/install_python_venv.ps1
+$this_noprompt_arg = if ($this_noprompt) {'-noprompt'} else {''}
+$venv_name_arg = if ($venv_name) {"-venv_name $venv_name"} else {''}
+. $rootPath/install_python_venv.ps1 $this_noprompt_arg $venv_name_arg
 
-# Execute the Python code using the specified interpreter
-try {
-    & $pythonExePath -c "import tkinter; print('Tkinter is available')"
-    Write-Host "Tkinter is available for $($pythonExePath)"
-} catch {
-    Write-Host "Tkinter is not available for $($pythonExePath)"
-    $venvPath = ""
-    if ($null -ne $env:VIRTUAL_ENV) {
-        Write-Host "A virtual environment is activated."
-        Write-Host "Virtual Environment Root: $($env:VIRTUAL_ENV)"
-        $venvPath = $env:VIRTUAL_ENV
-        deactivate
-    } else {
-        Write-Host "No virtual environment is activated."
-    }
-    if ((Get-OS) -eq "Mac") {
-        brew install python-tk
-    } elseif ((Get-OS) -eq "Linux") {
-        if (Test-Path -Path "/etc/os-release") {
-            $distro = (Get-Linux-Distro-Name)
-            $command = ""
-            switch ($distro) {
-                "debian" {
-                    $command = "sudo apt-get install python3-tk -y"
-                    break
-                }
-                "ubuntu" {
-                    $command = "sudo apt-get install python3-tk -y"
-                    break
-                }
-                "fedora" {
-                    $command = "sudo dnf install python3-tkinter python3.10-tkinter"
-                    break
-                }
-                "almalinux" {
-                    sudo dnf install tk-devel tcl-devel
-                    $command = "sudo dnf install python3-tkinter -y"
-                    break
-                }
-                "alpine" {
-                    $command = "sudo apk add ttf-dejavu fontconfig python3-tkinter"
-                    break
-                }
-                "arch" {
-                    $command = "sudo pacman -Sy tk mpdecimal --noconfirm"
-                }
-            }
-        
-            if ($command -eq "") {
-                Write-Warning "Dist '$distro' not supported for automated system package install, please install the dependencies if you experience problems."
-            } else {
-                Write-Host "Executing command: $command"
-                Invoke-Expression $command
-            }
-        }
-    }
-    if ($venvPath -ne "" -and $null -ne $venvPath) {
-        Write-Host "Deleting old venv at '$venvPath'..."
-        Remove-Item -Path $venvPath -Recurse -Force
-    }
-    Write-Host "Reinitializing python virtual environment..."
-    $this_noprompt_arg = if ($this_noprompt) {'-noprompt'} else {''}
-    $venv_name_arg = if ($venv_name) {"-venv_name $venv_name"} else {''}
-    . $rootPath/install_python_venv.ps1 $this_noprompt_arg $venv_name_arg
-}
-
-Write-Host "Installing required packages to build the batchpatcher..."
-. $pythonExePath -m pip install --upgrade pip --prefer-binary --progress-bar on
-. $pythonExePath -m pip install pyinstaller --prefer-binary --progress-bar on
-. $pythonExePath -m pip install -r ($rootPath + $pathSep + "Tools" + $pathSep + "BatchPatcher" + $pathSep + "requirements.txt") --prefer-binary --progress-bar on
-. $pythonExePath -m pip install -r ($rootPath + $pathSep + "Libraries" + $pathSep + "PyKotor" + $pathSep + "requirements.txt") --prefer-binary --progress-bar on
-. $pythonExePath -m pip install -r ($rootPath + $pathSep + "Libraries" + $pathSep + "PyKotorFont" + $pathSep + "requirements.txt") --prefer-binary --progress-bar on
-
+Write-Host "Initializing python virtual environment..."
+$this_noprompt_arg = if ($this_noprompt) {'-noprompt'} else {''}
+$venv_name_arg = if ($venv_name) {"-venv_name $venv_name"} else {''}
+. $rootPath/install_python_venv.ps1 $this_noprompt_arg $venv_name_arg
 
 $current_working_dir = (Get-Location).Path
 Set-Location -LiteralPath (Resolve-Path -LiteralPath "$rootPath/Tools/BatchPatcher/src").Path
