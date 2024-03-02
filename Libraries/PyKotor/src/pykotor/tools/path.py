7207057e800a68bfc845ea06488b382e6ce84e30
Benjamin Auquite <halomastar@gmail.com>
2024-03-02 17:10:20 -0600

    mark a MacOS kotor path as verified

--- Libraries/PyKotor/src/pykotor/tools/path.py
+++ Libraries/PyKotor/src/pykotor/tools/path.py
@@ -10,7 +10,6 @@ from pykotor.tools.registry import find_software_key, winreg_key
 from utility.string import ireplace
 from utility.system.path import (
     Path as InternalPath,
-    PosixPath as InternalPosixPath,
     PurePath as InternalPurePath,
     WindowsPath as InternalWindowsPath,
 )
@@ -366,7 +365,7 @@ def get_default_paths() -> dict[str, dict[Game, list[str]]]:
         },
         "Darwin": {
             Game.K1: [
-                "~/Library/Application Support/Steam/steamapps/common/swkotor/Knights of the Old Republic.app/Contents/Assets",
+                "~/Library/Application Support/Steam/steamapps/common/swkotor/Knights of the Old Republic.app/Contents/Assets",  # Verified
                 "~/Library/Applications/Steam/steamapps/common/swkotor/Knights of the Old Republic.app/Contents/Assets/",
                 # TODO: app store version of k1
             ],
