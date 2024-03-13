ae5cd777089b0381f96a497fdfeab65b52cae8a8
Benjamin Auquite <halomastar@gmail.com>
2024-03-13 07:52:51 -0500

    last known working version

--- install_powershell.sh
+++ install_powershell.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-# Default values
-noprompt=false
-
-# Parse command-line arguments
-while getopts ":n" opt; do
-    case ${opt} in
-        n )
-            noprompt=true
-            ;;
-        \? )
-            echo "Usage: $0 [-n]"
-            echo "[-n] - Don't prompt for any instruction (default is to prompt)"
-            exit 1
-            ;;
-    esac
-done
-#shift $((OPTIND -1)) uncomment if a second cmdline arg is needed
-
 # Function to install PowerShell on Linux
 fallback_install_pwsh() {
     if ! command -v pwsh > /dev/null; then
@@ -70,7 +52,9 @@ fallback_install_pwsh() {
     if ! command -v pwsh > /dev/null; then
         if command -v brew > /dev/null; then
             echo "Found 'brew' command"
-            install_powershell_brew
+            brew update
+            brew install powershell/tap/powershell
+            brew install --cask powershell
         fi
     fi
     #if ! command -v pwsh > /dev/null; then
@@ -173,91 +157,45 @@ install_powershell_archlinux() {
     }
 }
 
-# Install powershell without Homebrew
-install_powershell_mac_fallback() {
-    echo "Downloading and installing the powershell direct pkg"
-    # Detect architecture
-    ARCH=$(uname -m)
-    PS_VERSION="7.2.18"  # The latest stable LTS mac version of Powershell
-    PS_PKG_PREFIX="https://github.com/PowerShell/PowerShell/releases/download/v$PS_VERSION/"
-
-    # Apple Silicon Mac
-    if [ "$ARCH" = "arm64" ]; then
-        PKG_FILENAME="powershell-$PS_VERSION-osx-arm64.pkg"
-    # Intel Mac
-    elif [ "$ARCH" = "x86_64" ]; then
-        PKG_FILENAME="powershell-$PS_VERSION-osx-x64.pkg"
-    # Unknown architecture
-    else
-        echo "Unsupported architecture: $ARCH"
-        exit 1
-    fi
-
-    echo "Downloading $PKG_FILENAME from $PS_PKG_PREFIX... please wait..."
-    curl -L -o "$PKG_FILENAME" "$PS_PKG_PREFIX/$PKG_FILENAME"
-
-    full_version=$(sw_vers -productVersion)  # Get the macOS full version number
-    major_version=$(echo "$full_version" | cut -d '.' -f 1)  # Extract the major version number (before the first dot)
-    echo "Version of mac is $full_version major version $major_version on arch $ARCH"
-    if [[ $major_version -ge 11 ]]; then
-        echo "This MacOS is running Big Sur or later, so downloaded pwsh package is most likely quarantined, let's call xattr now"
-        sudo xattr -rd com.apple.quarantine $PKG_FILENAME
-    fi
-
-    echo "Installing $PKG_FILENAME"
-    sudo installer -pkg $PKG_FILENAME -target /
-}
-
-install_powershell_brew() {
-    brew update
-    if [ $? -ne 0 ]; then
-        echo "Error: 'brew update' failed."
-        install_powershell_mac_fallback
-    fi
-    brew install powershell/tap/powershell
-    if [ $? -ne 0 ]; then
-        echo "Error: 'brew install powershell/tap/powershell' failed."
-        install_powershell_mac_fallback
-    fi
-    brew install --cask powershell
-    if [ $? -ne 0 ]; then
-        echo "Error: 'brew install --cask powershell' failed."
-        install_powershell_mac_fallback
-    fi
-}
-
 # Function to install PowerShell on macOS
 install_powershell_mac() {
 
-    if ! command -v brew &>/dev/null; then
-
+    which -s brew
+    if [[ $? != 0 ]] ; then
         # Install Homebrew
-        echo "Installing Powershell through Homebrew is recommended (by microsoft themselves)"
-        echo "Install HomeBrew now? (y) otherwise attempt to fallback to direct pkg download (n)"
-        if [ $noprompt == false ]
-            read -p "Enter your choice (y/N): " user_choice
-        else
-            user_choice = "y"
-        fi
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> ~/.zprofile
+        eval $(/opt/homebrew/bin/brew shellenv)
+        which -s brew
+        if [[ $? != 0 ]] ; then
+            echo "Could not install homebrew on your system. Attempting to install powershell via another method..."
+            # Detect architecture
+            ARCH=$(uname -m)
+            PS_VERSION="7.2.17"  # The latest stable LTS mac version of Powershell
+            PS_PKG_PREFIX="https://github.com/PowerShell/PowerShell/releases/download/v$PS_VERSION/powershell-$PS_VERSION"
 
-        if [ $user_choice == "y" ] || [ $user_choice == "Y"]; then
-            echo "Installing HomeBrew... please wait..."
-            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-            echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> ~/.zprofile
-            eval $(/opt/homebrew/bin/brew shellenv)
-
-            if ! command -v brew &>/dev/null; then
-                echo "Could not install homebrew on your system. Attempting to install powershell via another method..."
-                install_powershell_mac_fallback
+            # Download the appropriate package based on architecture
+            if [ "$ARCH" == "arm64" ]; then
+                # Apple Silicon Mac
+                curl -L -o "powershell-$PS_VERSION-osx-arm64.pkg" "$PS_PKG_PREFIX-osx-arm64.pkg"
+            elif [ "$ARCH" == "x86_64" ]; then
+                # Intel Mac
+                curl -L -o "powershell-$PS_VERSION-osx-x64.pkg" "$PS_PKG_PREFIX-osx-x64.pkg"
             else
-                install_powershell_brew
+                echo "Unsupported architecture: $ARCH"
+                exit 1
             fi
+            sudo xattr -rd com.apple.quarantine ./powershell-$PS_VERSION-osx-$ARCH.pkg
+            sudo installer -pkg ./powershell-$PS_VERSION-osx-$ARCH.pkg -target /
         else
-            echo "Skipping install of HomeBrew and installing powershell directly from the pkg..."
-            install_powershell_mac_fallback
+            brew update
+            brew install powershell/tap/powershell
+            brew install --cask powershell
         fi
     else
-        install_powershell_brew
+        brew update
+        brew install powershell/tap/powershell
+        brew install --cask powershell
     fi
 }
 
@@ -266,18 +204,9 @@ install_powershell_debian() {
     sudo apt-get update
     sudo apt-get install -y wget
     source /etc/os-release
-    if ! command -v wget &>/dev/null; then
-        if ! command -v curl &>/dev/null; then
-            echo "Error: Neither wget nor curl is available. Cannot download the file."
-            exit 1
-        else
-            curl -L -o packages-microsoft-prod.deb https://packages.microsoft.com/config/debian/$VERSION_ID/packages-microsoft-prod.deb
-        fi
-    else
-        wget -q https://packages.microsoft.com/config/debian/$VERSION_ID/packages-microsoft-prod.deb
-    fi
+    wget -q https://packages.microsoft.com/config/debian/$VERSION_ID/packages-microsoft-prod.deb
     sudo dpkg -i packages-microsoft-prod.deb
-    rm -f packages-microsoft-prod.deb
+    rm packages-microsoft-prod.deb
     sudo apt-get update
     sudo apt-get install -y powershell
 }
@@ -383,38 +312,19 @@ case "$OS" in
 
                     # Compare version to see if it's less than 8.5
                     if [[ "$centos_version" -lt 85 ]]; then
-                        echo "CentOS version is less than 8.5, considering converting to AlmaLinux..."
+                        echo "CentOS version is less than 8.5, converting to alma linux..."
                     else
-                        echo "CentOS version is 8.5 or higher, considering converting to AlmaLinux..."
+                        echo "CentOS version is 8.5 or higher, converting to alma linux..."
                     fi
-                    echo "You are currently running CentOS version $centos_version. The package manager 'yum' on here reached EOL and is no longer hosted."
-                    echo "In order to install powershell, we must convert you to alma linux through their supported process (recommended)."
-                    echo "WARNING: Converting CentOS to AlmaLinux is a significant change and not reversible. This process will change your CentOS distribution to AlmaLinux to continue with the PowerShell installation."
-                    echo "Do you want to continue with the conversion?"
-                    if [ $noprompt == false ]
-                        read -p "Enter your choice (y/N): " user_choice
-                    else
-                        user_choice = "y"
-                    fi
-
-                    if [[ $user_choice == "y" ]]; then
-                        sed -i -r 's|^(mirrorlist.+)$|#\1|g; s|^#baseurl=http://mirror.centos.org/\$contentdir/\$releasever/|baseurl=https://vault.centos.org/8.5.2111/|g' /etc/yum.repos.d/CentOS-*.repo
-                        sudo yum update -y
-                        echo "Downloading AlmaLinux conversion script..."
-                        curl -O https://raw.githubusercontent.com/AlmaLinux/almalinux-deploy/master/almalinux-deploy.sh
-                        echo "Converting to AlmaLinux. This may take some time..."
-                        sudo bash almalinux-deploy.sh
-                        sudo rpm --import https://repo.almalinux.org/almalinux/RPM-GPG-KEY-AlmaLinux && sudo bash almalinux-deploy.sh
-                        echo "Conversion to AlmaLinux completed."
-                        cat /etc/redhat-release
-                        sudo grubby --info DEFAULT | grep AlmaLinux
-                        sudo yum update
-                        sudo yum install -y powershell
-                        echo "PowerShell has been installed successfully."
-                    else
-                        echo "Conversion canceled by the user. Attempting fallbacks..."
-                        fallback_install_pwsh
-                    fi
+                    sed -i -r 's|^(mirrorlist.+)$|#\1|g; s|^#baseurl=http://mirror.centos.org/\$contentdir/\$releasever/|baseurl=https://vault.centos.org/8.5.2111/|g' /etc/yum.repos.d/CentOS-*.repo
+                    sudo yum update -y
+                    curl -O https://raw.githubusercontent.com/AlmaLinux/almalinux-deploy/master/almalinux-deploy.sh
+                    sudo bash almalinux-deploy.sh
+                    sudo rpm --import https://repo.almalinux.org/almalinux/RPM-GPG-KEY-AlmaLinux && sudo bash almalinux-deploy.sh
+                    cat /etc/redhat-release
+                    sudo grubby --info DEFAULT | grep AlmaLinux
+                    sudo yum update
+                    sudo yum install -y powershell
                     ;;
                 almalinux)
                     echo "Installing Powershell for almalinux..."
