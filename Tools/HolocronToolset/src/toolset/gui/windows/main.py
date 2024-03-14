7de6f2901841d6137057679356f55e00d3f31eb7
Benjamin Auquite <halomastar@gmail.com>
2024-03-14 05:58:16 -0500

    Add more core resources into toolset.

--- Tools/HolocronToolset/src/toolset/gui/windows/main.py
+++ Tools/HolocronToolset/src/toolset/gui/windows/main.py
@@ -19,6 +19,7 @@ from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 
 from pykotor.common.stream import BinaryReader
+from pykotor.extract.capsule import Capsule
 from pykotor.extract.file import ResourceIdentifier
 from pykotor.extract.installation import SearchLocation
 from pykotor.resource.formats.erf.erf_auto import read_erf, write_erf
@@ -900,6 +901,13 @@ class ToolWindow(QMainWindow):
         else:
             print("Loading core installation resources into UI...")
             self.ui.coreWidget.setResources(self.active.chitin_resources())
+            if self.active.game().is_k1():
+                patch_erf_path = self.active.path() / "patch.erf"
+                if patch_erf_path.safe_isfile():
+                    self.ui.coreWidget.setResources(Capsule(patch_erf_path).resources(), clear_existing=False)
+            self.ui.coreWidget.setResources(self.active._streamwaves, "Stream Waves", clear_existing=False)
+            self.ui.coreWidget.setResources(self.active._streammusic, "Stream Music", clear_existing=False)
+            self.ui.coreWidget.setResources(self.active._streamsounds, "Stream Sounds", clear_existing=False)
             print("Loading module resources into UI...")
             self.refreshModuleList(reload=False)
             print("Loading override resources into UI...")
