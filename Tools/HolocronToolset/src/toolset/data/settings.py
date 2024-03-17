e21ad68861f03793817d1245cf4b33c93ec93b34
Benjamin Auquite <halomastar@gmail.com>
2024-03-17 11:10:02 -0500

    QVariant not supported.

--- Tools/HolocronToolset/src/toolset/data/settings.py
+++ Tools/HolocronToolset/src/toolset/data/settings.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 from typing import Any
 
-from qtpy.QtCore import QSettings
+import jsonpickle
+
+from qtpy.QtCore import QSettings, Qt
 
 
 class Settings:
@@ -10,8 +12,30 @@ class Settings:
         self.settings = QSettings("HolocronToolset", scope)
 
     @staticmethod
-    def addSetting(name: str, default: Any):
-        return property(
-            lambda this: this.settings.value(name, default, default.__class__),
-            lambda this, val: this.settings.setValue(name, val),
-        )
+    def addSetting(name: str, default: Any) -> property:
+        # sourcery skip: remove-unnecessary-cast
+        def convert_value(value):
+            """Recursively converts values, including Qt.Key and nested structures, for serialization."""
+            if isinstance(value, (Qt.Key, Qt.MouseButton)):
+                return int(value)  # Convert Qt.Key to int
+            elif isinstance(value, set):
+                return [convert_value(item) for item in value]  # Convert each item in a set
+            elif isinstance(value, tuple):
+                return tuple(convert_value(item) for item in value)  # Convert each item in a tuple
+            elif isinstance(value, list):
+                return [convert_value(item) for item in value]  # Convert each item in a list
+            elif isinstance(value, dict):
+                return {key: convert_value(val) for key, val in value.items()}  # Convert each value in a dict
+            else:
+                return value
+
+        def getter(this: Settings) -> Any:
+            l_default = convert_value(default)
+            serialized_default = jsonpickle.encode(l_default)
+            return jsonpickle.decode(this.settings.value(name, serialized_default, serialized_default.__class__))  # noqa: S301
+
+        def setter(this: Settings, value: Any):
+            #serialized_value = jsonpickle.encode(value, warn=True)
+            this.settings.setValue(name, jsonpickle.encode(convert_value(value)))
+
+        return property(getter, setter)
