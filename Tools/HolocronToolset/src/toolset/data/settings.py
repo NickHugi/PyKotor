64199ad4250c86348892b6a655e256f174ed33b8
Benjamin Auquite <halomastar@gmail.com>
2024-03-19 13:07:09 -0500

    fallback to whatever qvariant was spitting out

--- Tools/HolocronToolset/src/toolset/data/settings.py
+++ Tools/HolocronToolset/src/toolset/data/settings.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from json import JSONDecodeError
 from typing import Any
 
 import jsonpickle
@@ -19,7 +20,7 @@ class Settings:
             if isinstance(value, (Qt.Key, Qt.MouseButton)):
                 return int(value)  # Convert Qt.Key to int
             elif isinstance(value, set):
-                return [convert_value(item) for item in value]  # Convert each item in a set
+                return {convert_value(item) for item in value}  # Convert each item in a set
             elif isinstance(value, tuple):
                 return tuple(convert_value(item) for item in value)  # Convert each item in a tuple
             elif isinstance(value, list):
@@ -32,7 +33,10 @@ class Settings:
         def getter(this: Settings) -> Any:
             l_default = convert_value(default)
             serialized_default = jsonpickle.encode(l_default)
-            return jsonpickle.decode(this.settings.value(name, serialized_default, serialized_default.__class__))  # noqa: S301
+            try:
+                return jsonpickle.decode(this.settings.value(name, serialized_default, serialized_default.__class__))  # noqa: S301
+            except JSONDecodeError as e:
+                return this.settings.value(name, l_default, l_default.__class__)
 
         def setter(this: Settings, value: Any):
             #serialized_value = jsonpickle.encode(value, warn=True)
