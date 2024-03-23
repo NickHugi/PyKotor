ecaaffdb1708655b622450a30693ddefe35dc08d
Benjamin Auquite <halomastar@gmail.com>
2024-03-23 10:47:16 -0500

    fix unix-specific problem with multiprocessing.

--- Tools/HolocronToolset/src/toolset/__main__.py
+++ Tools/HolocronToolset/src/toolset/__main__.py
@@ -97,6 +97,7 @@ if __name__ == "__main__":
         multiprocessing.freeze_support()
     else:
         fix_sys_and_cwd_path()
+    multiprocessing.set_start_method("spawn")
 
     from utility.system.path import Path
 
