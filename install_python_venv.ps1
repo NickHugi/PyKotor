665d29cd13c122cb84f815a479c411b27b950a09
Benjamin Auquite <halomastar@gmail.com>
2024-03-28 20:36:11 -0500

    pwsh: fix infinite loop on ubuntu/debian

--- install_python_venv.ps1
+++ install_python_venv.ps1
@@ -862,7 +862,7 @@ function Find-Python {
     # Example: while a venv can be partially created, the 
     # partial won't create stuff like the activation scripts (so they need sudo apt-get install python3-venv)
     # they'll also be missing things like pip. This step fixes that.
-    if ((Get-Linux-Distro-Name) -eq "debian" -or (Get-Linux-Distro-Name) -eq "ubuntu") {
+    if ($installIfNotFound -and ((Get-Linux-Distro-Name) -eq "debian" -or (Get-Linux-Distro-Name) -eq "ubuntu")) {
         $versionTypeObj = New-Object -TypeName "System.Version" $global:pythonVersion
         $shortVersion = "{0}.{1}" -f $versionTypeObj.Major, $versionTypeObj.Minor
         Install-Python-Linux -pythonVersion $shortVersion
