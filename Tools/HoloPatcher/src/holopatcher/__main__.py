1464492ee66ae9c435394f79860ab397654caad2
Benjamin Auquite <halomastar@gmail.com>
2024-05-12 05:02:49 -0500

    Update __main__.py

--- Tools/HoloPatcher/src/holopatcher/__main__.py
+++ Tools/HoloPatcher/src/holopatcher/__main__.py
@@ -55,7 +55,6 @@ if not is_frozen():
         if utility_path.exists():
             update_sys_path(utility_path.parent)
 
-from types import TracebackType
 
 from holopatcher.config import CURRENT_VERSION, getRemoteHolopatcherUpdateInfo, remoteVersionNewer
 from pykotor.common.misc import Game
