ea8a57831fa58a27a0c53e432145ea5ee293757f
Benjamin Auquite <halomastar@gmail.com>
2024-05-20 12:43:00 -0500

    bump everyone to update to the hotfix

--- Tools/HolocronToolset/src/toolset/config.py
+++ Tools/HolocronToolset/src/toolset/config.py
@@ -17,7 +17,7 @@ LOCAL_PROGRAM_INFO: dict[str, Any] = {
     # <---JSON_START--->#{
     "currentVersion": "3.0.0b8.1",
     "toolsetLatestVersion": "2.1.2",
-    "toolsetLatestBetaVersion": "3.0.0b8",
+    "toolsetLatestBetaVersion": "3.0.0b8.1",
     "updateInfoLink": "https://api.github.com/repos/NickHugi/PyKotor/contents/Tools/HolocronToolset/src/toolset/config.py",
     "updateBetaInfoLink": "https://api.github.com/repos/NickHugi/PyKotor/contents/Tools/HolocronToolset/src/toolset/config.py?ref=bleeding-edge",
     "toolsetDownloadLink": "https://deadlystream.com/files/file/1982-holocron-toolset",
