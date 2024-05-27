12915d23f763f268f9bd695e7be1a1c82972ed5d
Benjamin Auquite <halomastar@gmail.com>
2024-05-26 23:09:10 -0500

    Update placeable.py

--- Libraries/PyKotor/src/pykotor/tools/placeable.py
+++ Libraries/PyKotor/src/pykotor/tools/placeable.py
@@ -2,20 +2,20 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from pykotor.resource.formats.twoda import read_2da
+from pykotor.resource.formats.twoda import TwoDA, read_2da
 from pykotor.resource.type import ResourceType
 
 if TYPE_CHECKING:
     from pykotor.extract.installation import Installation
-    from pykotor.resource.formats.twoda import TwoDA
     from pykotor.resource.generics.utp import UTP
+    from pykotor.resource.type import SOURCE_TYPES
 
 
 def get_model(
     utp: UTP,
     installation: Installation,
     *,
-    placeables: TwoDA | None = None,
+    placeables: TwoDA | SOURCE_TYPES | None = None,
 ) -> str:
     """Returns the model name for the given placeable.
 
@@ -23,7 +23,7 @@ def get_model(
 
     Args:
     ----
-        utp: UTC object of the target placeable.
+        utp: UTP object of the placeable to lookup the model for.
         installation: The relevant installation.
         placeables: The placeables.2da loaded into a TwoDA object.
 
@@ -32,6 +32,11 @@ def get_model(
         Returns the model name for the placeable.
     """
     if placeables is None:
-        placeables = read_2da(installation.resource("genericdoors", ResourceType.TwoDA).data)
+        result = installation.resource("placeables", ResourceType.TwoDA)
+        if not result:
+            raise ValueError("Resource 'placeables.2da' not found in the installation, cannot get UTP model.")
+        placeables = read_2da(result.data)
+    elif not isinstance(placeables, TwoDA):
+        placeables = read_2da(placeables)
 
     return placeables.get_row(utp.appearance_id).get_string("modelname")
