2017613845756d3324e9591f6c18d3e9409954cb
Benjamin Auquite <halomastar@gmail.com>
2024-08-02 22:57:16 -0500

    cleanup/fix hwnd/win32 context menu

--- Libraries/Utility/src/utility/system/win32/hwnd.py
+++ Libraries/Utility/src/utility/system/win32/hwnd.py
@@ -1,76 +1,131 @@
 from __future__ import annotations
 
-import ctypes
-
-from ctypes import windll
+from ctypes import (
+    WINFUNCTYPE,
+    Structure,
+    c_int,
+    c_long,
+    c_uint,
+    c_void_p,
+    windll,
+)
+from ctypes.wintypes import HBRUSH, HICON, HINSTANCE, HWND, LPARAM, LPCWSTR, UINT, WPARAM
+from enum import IntEnum
 from typing import TYPE_CHECKING, Sequence
 
+import win32gui
+
 if TYPE_CHECKING:
-    from ctypes import _CData
+    from ctypes import (
+        _CData,
+        _FuncPointer,
+    )
+    from ctypes.wintypes import HMENU, LPVOID
     from types import TracebackType
 
     from _win32typing import PyResourceId  # pyright: ignore[reportMissingModuleSource]
 
-try:
-    from ctypes.wintypes import HWND, LPARAM
-except Exception:  # noqa: BLE001  # pyinstaller frozen-related issue observed.
-    # just grab from ctypes.wintypes src I suppose
-    # WPARAM is defined as UINT_PTR (unsigned type)
-    # LPARAM is defined as LONG_PTR (signed type)
-    if ctypes.sizeof(ctypes.c_long) == ctypes.sizeof(ctypes.c_void_p):
-        WPARAM = ctypes.c_ulong
-        LPARAM = ctypes.c_long
-    elif ctypes.sizeof(ctypes.c_longlong) == ctypes.sizeof(ctypes.c_void_p):
-        WPARAM = ctypes.c_ulonglong
-        LPARAM = ctypes.c_longlong
-    HWND = ctypes.c_void_p
 
-
-# Define window class and procedure
-WNDPROC: type[ctypes._FuncPointer] = ctypes.WINFUNCTYPE(ctypes.c_long, HWND, ctypes.c_uint, ctypes.c_uint, LPARAM)
-
-# Windows constants
+WNDPROC: type[_FuncPointer] = WINFUNCTYPE(c_long, HWND, c_uint, WPARAM, LPARAM)
 WM_DESTROY = 0x0002
-IDC_ARROW = 32512
-CS_HREDRAW = 0x0002
-CS_VREDRAW = 0x0001
+WS_OVERLAPPEDWINDOW = (0x00CF0000)
+WS_VISIBLE = 0x10000000
+HCURSOR = c_void_p
 
 
-class WNDCLASS(ctypes.Structure):
+class ctypesWNDCLASS(Structure):  # noqa: N801
     _fields_: Sequence[tuple[str, type[_CData]] | tuple[str, type[_CData], int]] = [
-        ("style", ctypes.c_uint),
+        ("style", UINT),
         ("lpfnWndProc", WNDPROC),
-        ("cbClsExtra", ctypes.c_int),
-        ("cbWndExtra", ctypes.c_int),
-        ("hInstance", ctypes.c_void_p),
-        ("hIcon", ctypes.c_void_p),
-        ("hCursor", ctypes.c_void_p),
-        ("hbrBackground", ctypes.c_void_p),
-        ("lpszMenuName", ctypes.c_wchar_p),
-        ("lpszClassName", ctypes.c_wchar_p),
+        ("cbClsExtra", c_int),
+        ("cbWndExtra", c_int),
+        ("hInstance", HINSTANCE),
+        ("hIcon", HICON),
+        ("hCursor", HCURSOR),
+        ("hbrBackground", HBRUSH),
+        ("lpszMenuName", LPCWSTR),
+        ("lpszClassName", LPCWSTR)
     ]
 
 
-def wnd_proc(hwnd: int | None, message: int, wparam: float | None, lparam: float | None) -> int:
+def wnd_proc(
+    hwnd: HWND,
+    message: c_uint,
+    wparam: WPARAM,
+    lparam: LPARAM,
+) -> c_long:
     if message == WM_DESTROY:
         windll.user32.PostQuitMessage(0)
-        return 0
-    return windll.user32.DefWindowProcW(hwnd, message, wparam, lparam)
+        return c_long(0)
+    print(f"wnd_proc(hwnd={hwnd}, message={message}, wparam={wparam}, lparam={lparam})")
+    result = windll.user32.DefWindowProcW(hwnd, message, wparam, lparam)
+    if isinstance(result, int):
+        return c_long(result)
+    print(result, "result is unexpectedly class:", result.__class__.__name__)
+    return result
 
 
-class HInvisibleWindow:
-    """Context manager for creating and destroying an invisible window."""
-    CLASS_NAME: str = "HInvisibleWindow"
-    DISPLAY_NAME: str = "Python Invisible Window"
+class CursorType(IntEnum):
+    ARROW = 32512
+    IBEAM = 32513
+    WAIT = 32514
+    CROSS = 32515
+    UPARROW = 32516
+    SIZE = 32640
+    ICON = 32641
+    SIZENWSE = 32642
+    SIZENESW = 32643
+    SIZEWE = 32644
+    SIZENS = 32645
+    SIZEALL = 32646
+    NO = 32648
+    HAND = 32649
+    APPSTARTING = 32650
+    HELP = 32651
 
-    def __init__(self):
+
+class SimplePyHWND:
+    """Context manager for creating and destroying a simple custom window."""
+    CLASS_NAME: str = "SimplePyHWND"
+    DISPLAY_NAME: str = "Python Simple Window"
+
+    def __init__(
+        self,
+        *,
+        visible: bool = False,
+        dwExStyle: int = 0,  # noqa: N803
+        lpClassName: PyResourceId | str | None = None,  # noqa: N803
+        lpWindowName: str | None = None,  # noqa: N803
+        dwStyle: int | None = None,  # noqa: N803
+        x: int = 0,
+        y: int = 0,
+        nWidth: int = 1280,  # noqa: N803
+        nHeight: int = 720,  # noqa: N803
+        hWndParent: HWND | None = None,  # noqa: N803
+        hMenu: HMENU | None = None,  # noqa: N803
+        hInstance: HINSTANCE | None = None,  # noqa: N803
+        lpParam: LPVOID | None = None,  # noqa: N803
+    ):
         self.hwnd: int | None = None
+        self.visible: bool = visible
+        self.dwExStyle: int = dwExStyle
+        self.lpClassName: PyResourceId | str | None = lpClassName or self.CLASS_NAME
+        self.lpWindowName: str | None = lpWindowName or self.DISPLAY_NAME
+        self.dwStyle: int | None = WS_OVERLAPPEDWINDOW | WS_VISIBLE if visible else (dwStyle or 0)
+        self.x: int = x
+        self.y: int = y
+        self.nWidth: int = nWidth
+        self.nHeight: int = nHeight
+        self.hWndParent: HWND | None = hWndParent
+        self.hMenu: HMENU | None = hMenu
+        self.hInstance: HINSTANCE | None = hInstance
+        self.lpParam: LPVOID | None = lpParam
 
     def __enter__(self) -> int:
         self.register_class()
         self.hwnd = self.create_window()
+        self._class_atom: PyResourceId | None = None
         return self.hwnd
-
 
     def __exit__(
         self,
@@ -84,24 +139,37 @@ class HInvisibleWindow:
 
     def register_class(self):
         """Register the window class."""
-        wc = WNDCLASS()
-        wc.lpfnWndProc = WNDPROC(wnd_proc)
-        wc.lpszClassName = self.CLASS_NAME
-        wc.hInstance = windll.kernel32.GetModuleHandleW(None)
-        wc.hCursor = windll.user32.LoadCursorW(None, IDC_ARROW)
-        wc.style = CS_HREDRAW | CS_VREDRAW
+        wc = win32gui.WNDCLASS()
+        wc.lpfnWndProc = WNDPROC(wnd_proc)  # pyright: ignore[reportAttributeAccessIssue]
+        wc.lpszClassName = self.CLASS_NAME  # pyright: ignore[reportAttributeAccessIssue]
+        self.hinst = wc.hInstance = win32gui.GetModuleHandle(None)  # pyright: ignore[reportAttributeAccessIssue]
+        wc.hCursor = windll.user32.LoadCursorW(None, CursorType.ARROW.value)  # pyright: ignore[reportAttributeAccessIssue]
         try:
-            self._class_atom: PyResourceId = windll.user32.RegisterClassW(ctypes.byref(wc))
-        except Exception as e:
+            self._class_atom = win32gui.RegisterClass(wc)
+        except Exception as e:  # pywintypes.error
             if getattr(e, "winerror", None) != 1410:  # class already registered
                 raise
+            print(f"{e} (Class already registered)")
 
     def unregister_class(self):
         """Unregister the window class."""
-        windll.user32.UnregisterClassW(self.CLASS_NAME, windll.kernel32.GetModuleHandleW(None))
+        if self._class_atom is not None:
+            win32gui.UnregisterClass(self._class_atom, windll.kernel32.GetModuleHandleW(None))
 
     def create_window(self) -> int:
-        """Create an invisible window."""
+        """Create the window."""
         return windll.user32.CreateWindowExW(
-            0, self.CLASS_NAME, self.DISPLAY_NAME, 0, 0, 0, 0, 0, None, None,
-            windll.kernel32.GetModuleHandleW(None), None)
+            self.dwExStyle, self.lpClassName, self.lpWindowName, self.dwStyle,
+            self.x, self.y, self.nWidth, self.nHeight,
+            self.hWndParent, self.hMenu,
+            windll.kernel32.GetModuleHandleW(None) if self.hInstance is None else self.hInstance,
+            self.lpParam)
+
+
+if __name__ == "__main__":
+    import time
+
+    with SimplePyHWND(visible=True) as hwnd:
+        print(f"Created SimplePyHWND with handle: {hwnd}")
+        time.sleep(3)
+    print("SimplePyHWND has been destroyed.")
--- Libraries/Utility/src/utility/system/win32/windows_context_menu.py
+++ Libraries/Utility/src/utility/system/win32/windows_context_menu.py
@@ -1,21 +1,23 @@
 from __future__ import annotations
 
-import sys
+import ctypes
+import errno
+import os
 
-from contextlib import nullcontext
+from contextlib import ExitStack
+from ctypes import byref, windll
 from pathlib import WindowsPath
-from typing import TYPE_CHECKING, Any, Sequence
+from typing import TYPE_CHECKING, Protocol, cast, runtime_checkable
 
-import win32com.client
-import win32con
-import win32gui
+import comtypes  # pyright: ignore[reportMissingTypeStubs]
+import comtypes.client  # pyright: ignore[reportMissingTypeStubs]
 
-from utility.system.win32.hwnd import HInvisibleWindow
+from utility.system.win32.hwnd import RobustInvisibleWindow, SimplePyHWND
 
 if TYPE_CHECKING:
-    import os
+    from typing import Any, Callable, Sequence
 
-    from win32com.client import DispatchBaseClass
+    from typing_extensions import Literal
     from win32com.client.dynamic import CDispatch
 
 
@@ -24,7 +26,8 @@ def safe_isfile(path: WindowsPath) -> bool | None:
         result: bool = path.is_file()
     except (OSError, ValueError):
         return None
-    return result
+    else:
+        return result
 
 
 def safe_isdir(path: WindowsPath) -> bool | None:
@@ -32,98 +35,158 @@ def safe_isdir(path: WindowsPath) -> bool | None:
         result: bool = path.is_dir()
     except (OSError, ValueError):
         return None
-    return result
+    else:
+        return result
 
 
-def show_context_menu(context_menu: CDispatch, hwnd: int | None):
-    hmenu: int = win32gui.CreatePopupMenu()
+def create_dispatch_shell() -> CDispatch | ShellNamespace:
+    try:
+        import win32com.client
+    except ImportError:
+        return comtypes.client.CreateObject("Shell.Application")
+    else:
+        return win32com.client.Dispatch("Shell.Application")
+
+
+def get_context_menu_functions()-> tuple[bool, Callable[..., Any], Callable[..., Any], Callable[..., Any], Callable[..., Any], int, int, int]:
+    try:
+        import win32con
+        import win32gui
+    except ImportError:
+        return (False, windll.user32.AppendMenuW, windll.user32.CreatePopupMenu, windll.user32.GetCursorPos, windll.user32.TrackPopupMenu,
+                0x0000, 0x0000, 0x0100)
+    else:
+        return (True, win32gui.AppendMenu, win32gui.CreatePopupMenu, win32gui.GetCursorPos, win32gui.TrackPopupMenu,
+            win32con.MF_STRING, win32con.TPM_LEFTALIGN, win32con.TPM_RETURNCMD)
+
+
+class _Vector2:
+    def __init__(self, x: int, y: int):
+        self.x: int = x
+        self.y: int = y
+
+
+class _POINT(ctypes.Structure):
+    _fields_ = [("x", ctypes.c_long), ("y", ctypes.c_long)]  # noqa: RUF012
+
+
+@runtime_checkable
+class ShellNamespace(Protocol):
+    def NameSpace(self, folder: str | Literal[0]) -> ShellFolder:
+        ...
+
+
+@runtime_checkable
+class ShellFolder(Protocol):
+    def ParseName(self, name: str) -> ShellFolderItem:
+        ...
+
+
+@runtime_checkable
+class ShellFolderItem(Protocol):
+    def Verbs(self) -> ShellFolderItemVerbs:
+        ...
+
+
+@runtime_checkable
+class ShellFolderItemVerbs(Protocol):
+    def Item(self, index: int) -> ShellFolderItemVerb:
+        ...
+    def __getitem__(self, index: int) -> ShellFolderItemVerb:
+        ...
+    def __len__(self) -> int:
+        ...
+
+
+@runtime_checkable
+class ShellFolderItemVerb(Protocol):
+    def DoIt(self) -> None:
+        ...
+    @property
+    def Name(self) -> str:
+        ...
+
+
+def get_cursor_pos(c_getcursorpos: Callable, *, use_pywin32: bool) -> _Vector2:
+    if use_pywin32:
+        return _Vector2(*c_getcursorpos())
+    pt = _POINT()
+    c_getcursorpos(byref(pt))
+    return cast(_Vector2, pt)
+
+
+def show_context_menu(context_menu: CDispatch | ShellFolderItemVerbs, hwnd: int | None):
+    # assert isinstance(context_menu, Iterable)  # this fails!
+    assert hasattr(context_menu, "__iter__")  # this also fails!
+    if not hasattr(context_menu, "__getitem__"):
+        raise TypeError(f"Expected arg1 to be Iterable or something similar: {context_menu} ({context_menu.__class__.__name__})")
+
+    pywin32_available, AppendMenu, CreatePopupMenu, GetCursorPos, TrackPopupMenu, MF_STRING, TPM_LEFTALIGN, TPM_RETURNCMD = get_context_menu_functions()
+    hmenu = CreatePopupMenu()
     for i, verb in enumerate(context_menu):  # pyright: ignore[reportArgumentType]
         if verb.Name:
-            win32gui.AppendMenu(hmenu, win32con.MF_STRING, i + 1, verb.Name)  # pyright: ignore[reportCallIssue]
-
-    pt: tuple[int, int] = win32gui.GetCursorPos()
-
-    context = HInvisibleWindow() if hwnd is None else nullcontext(hwnd)
-    with context as hwnd:
-        cmd: Any = win32gui.TrackPopupMenu(
-            hmenu, win32con.TPM_LEFTALIGN | win32con.TPM_RETURNCMD,
-            pt[0], pt[1], 0,
-            hwnd, None  # pyright: ignore[reportArgumentType]
-        )
-    if isinstance(cmd, int):
-        verb: DispatchBaseClass = context_menu.Item(cmd - 1)
-        if verb:
-            verb.DoIt()
+            AppendMenu(hmenu, MF_STRING, i + 1, verb.Name)
+    pt: _Vector2 = get_cursor_pos(GetCursorPos, use_pywin32=pywin32_available)
+    with ExitStack() as stack:
+        hwnd = stack.enter_context(RobustInvisibleWindow()) if hwnd is None else hwnd
+        cmd = TrackPopupMenu(hmenu, TPM_LEFTALIGN | TPM_RETURNCMD,
+                             pt.x, pt.y, 0, hwnd, None)
+    if not isinstance(cmd, int):
+        raise RuntimeError("Unable to open the context manager, reason unknown")  # noqa: TRY004
+    verb = context_menu.Item(cmd - 1)
+    if verb:
+        verb.DoIt()
 
 
-# Function to display context menu
 def windows_context_menu_file(
     file_path: os.PathLike | str,
     hwnd: int | None = None,
-):
-    """Opens the default windows context menu for a filepath at the position of the cursor."""
-    parsed_filepath: WindowsPath = WindowsPath(file_path)
-
-    shell = win32com.client.Dispatch("Shell.Application")
-    folder: CDispatch = shell.NameSpace(str(parsed_filepath.parent))
-    item: CDispatch = folder.ParseName(parsed_filepath.name)
-    context_menu: CDispatch = item.Verbs()
-
-    show_context_menu(context_menu, hwnd)
-
-
-def windows_context_menu_folder(
-    folder_path: os.PathLike | str,
-    hwnd: int | None = None,
-):
-    """Opens the default windows context menu for a folderpath at the position of the cursor."""
-    shell = win32com.client.Dispatch("Shell.Application")
-    folder: CDispatch = shell.NameSpace(str(WindowsPath(folder_path)))
-    item: CDispatch = folder.Self
-    context_menu: CDispatch = item.Verbs()
-
-    show_context_menu(context_menu, hwnd)
-
-
-def windows_context_menu(
-    path: os.PathLike | str,
-    hwnd: int | None = None,
-):
-    """Opens the default windows context menu for a folder/file path at the position of the cursor."""
-    parsed_path = WindowsPath(path)
-    if safe_isfile(parsed_path):
-        windows_context_menu_file(parsed_path, hwnd)
-    elif safe_isdir(parsed_path):
-        windows_context_menu_folder(parsed_path, hwnd)
-    else:
-        msg = f"Path is neither file nor folder: '{path}'"
-        raise ValueError(msg)
+) -> None:
+    parsed_filepath: WindowsPath = WindowsPath(file_path).resolve()
+    shell = create_dispatch_shell()
+    folder_object = shell.NameSpace(str(parsed_filepath.parent))
+    folder_item = folder_object.ParseName(parsed_filepath.name)
+    show_context_menu(folder_item.Verbs(), hwnd)
 
 
 def windows_context_menu_multiple(
     paths: Sequence[os.PathLike | str],
     hwnd: int | None = None,
 ):
-    """Opens the default windows context menu for multiple files/folder paths at the position of the cursor."""
-    shell = win32com.client.Dispatch("Shell.Application")
-    folders_items: list[CDispatch] = [
-        shell.NameSpace(str(path.parent if safe_isfile(path) else path)).ParseName(path.name)
-        for path in (WindowsPath(path) for path in paths)
-    ]
-    context_menu: CDispatch = folders_items[0].Verbs()
+    parsed_paths: list[WindowsPath] = [WindowsPath(path).resolve() for path in paths]
+    folder_items = []
+    shell = create_dispatch_shell()
+    for path in parsed_paths:
+        folder_object = shell.NameSpace(str(path.parent if safe_isfile(path) else path))
+        item = folder_object.ParseName(path.name)
+        folder_items.append(item)
+    show_context_menu(folder_items[0].Verbs(), hwnd)
 
+
+def windows_context_menu_folder(
+    folder_path: os.PathLike | str,
+    hwnd: int | None = None,
+) -> None:
+    parsed_folderpath: WindowsPath = WindowsPath(folder_path).resolve()
+    shell = create_dispatch_shell()
+    desktop_object = shell.NameSpace(0)
+    folder_item = desktop_object.ParseName(str(parsed_folderpath))
+    context_menu = folder_item.Verbs()
     show_context_menu(context_menu, hwnd)
 
+def windows_context_menu(path: os.PathLike | str, hwnd: int | None = None):
+    parsed_path: WindowsPath = WindowsPath(path)
+    if safe_isfile(parsed_path):
+        windows_context_menu_file(parsed_path, hwnd)
+    elif safe_isdir(parsed_path):
+        windows_context_menu_folder(parsed_path, hwnd)
+    else:
+        msg = f"Path is neither file nor folder: {path}"
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), msg)
 
 # Example usage
 if __name__ == "__main__":
-    import sys
-
-    from toolset.__main__ import onAppCrash
-
-    sys.excepthook = onAppCrash
-    filepath = r"C:\Users\Wizard\test_folder\City.sol"
-    windows_context_menu(filepath)
+    windows_context_menu(r"C:\Users\Wizard\test_folder\City.sol")
 
     multiple_files = [
         r"C:\Users\Wizard\test_folder\RestoreBackup.ps1",
